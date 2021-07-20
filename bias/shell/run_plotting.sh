e9cc390a797f73c0d39f7aac7c7abe3c2c577930
Hiyam G <hkg02@mail.aub.edu>
2021-07-20 10:33:16 +0300

    reduced number of cpus per task

--- bias/shell/run_plotting.sh
+++ bias/shell/run_plotting.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=normal
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=1
 #SBATCH --mem=16000
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=hkg02@mail.aub.edu
