52e107776c1ca5b45078520f7efc44b5e30f3d11
Hiyam G <hkg02@mail.aub.edu>
2021-07-20 21:15:49 +0300

    refactored script for writing keywords into lines with commas

--- bias/transform_into_lines.py
+++ bias/transform_into_lines.py
@@ -1,5 +1,3 @@
-import os
-
 
 def print_lines(file_path):
     with open(file_path, 'r', encoding='utf-8') as f:
@@ -9,29 +7,18 @@ def print_lines(file_path):
 
 
 if __name__ == '__main__':
-    # participants_israel = 'participants_aspect/participants_israel.txt'
-    # print_lines(participants_israel)
-
-    # participants_palestine = 'participants_aspect/participants_palestine.txt'
-    # print_lines(participants_palestine)
-
-    # participants_other = 'participants_aspect/other_participants.txt'
-    # print_lines(participants_other)
-
-    # agents_violence = 'military_aspect/agents_of_violence.txt'
-    # print_lines(agents_violence)
-
-    # methods_violence = 'military_aspect/methods_of_violence.txt'
-    # print_lines(methods_violence)
-
-    # outcomes_violence = 'military_aspect/outcomes_of_violence.txt'
-    # print_lines(outcomes_violence)
-
-    # break_violence = 'military_aspect/break_from_violence.txt'
-    # print_lines(break_violence)
-
-    # occupation_practices = 'Israeli_Occupation_Practices/israeli_occupation_practices.txt'
-    # print_lines(occupation_practices)
-
-    occupation_practices = 'israeli_palestinian_conflict/occupations_vs_peace+israel/israel_list_arabic.txt'
-    print_lines(occupation_practices)
+    participants_israel = 'keywords/arabic/participants_Israel_arabic.txt'
+    print_lines(participants_israel)
+    print('==================================')
+    participants_palestine = 'keywords/arabic/participants_palestine_arabic.txt'
+    print_lines(participants_palestine)
+    print('==================================')
+    terrorism = 'keywords/arabic/terrorism(100yearsofbias)_arabic.txt'
+    print_lines(terrorism)
+    print('==================================')
+    methods_violence = 'keywords/arabic/methods_of_violence_arabic.txt'
+    print_lines(methods_violence)
+    print('==================================')
+    peace_practices = 'keywords/arabic/non_occupation_practices_arabic.txt'
+    print_lines(peace_practices)
+    print('==================================')
