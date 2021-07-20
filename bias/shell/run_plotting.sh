6ac6da56e0a1e9b0a4be26a2ed715335394a662a
Hiyam G <hkg02@mail.aub.edu>
2021-07-20 13:41:48 +0300

    made script to run on GPU

--- bias/shell/run_plotting.sh
+++ bias/shell/run_plotting.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 #SBATCH --job-name=bias
 #SBATCH --account=hkg02
-#SBATCH --partition=normal
+#SBATCH --partition=gpu
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=16000
+#SBATCH --cpus-per-task=2
 #SBATCH --mail-type=ALL
+#SBATCH --gres=gpu
 #SBATCH --mail-user=hkg02@mail.aub.edu
 
 module load python/3
