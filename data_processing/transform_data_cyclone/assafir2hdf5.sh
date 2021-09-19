bb98593f551ad83b7adfae95284cf407fafaa051
Hiyam G <hkg02@mail.aub.edu>
2021-09-19 14:14:34 +0300

    refactored gpu specification in bash script

--- data_processing/transform_data_cyclone/assafir2hdf5.sh
+++ data_processing/transform_data_cyclone/assafir2hdf5.sh
@@ -3,8 +3,7 @@
 #SBATCH --account=p078
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=hkg02@mail.aub.edu
 
