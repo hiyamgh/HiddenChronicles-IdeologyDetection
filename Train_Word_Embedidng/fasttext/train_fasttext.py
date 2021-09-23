ac4f81b2aa8c9eaddb14b0030dfeef8ace8144af
hiyamgh <hkg02@mail.aub.edu>
2021-09-23 13:46:47 +0300

    updated scripts to include training with SLURM job arrays

--- Train_Word_Embedidng/fasttext/train_fasttext.py
+++ Train_Word_Embedidng/fasttext/train_fasttext.py
@@ -2,6 +2,7 @@ import fasttext
 import argparse
 import os, logging
 import multiprocessing as mp
+from multiprocessing import Pool, Process, Queue
 
 
 def mkdir(folder):
@@ -20,13 +21,15 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--dim', type=int, help='size of word vectors')
     parser.add_argument('-s', '--ws', type=int, help='size of the context window')
     parser.add_argument('-n', '--neg', type=int, help='number of negatives sampled')
+    parser.add_argument('-y', '--year', type=int, help='year to train on')
     args = parser.parse_args()
 
     archive = args.archive
     data_folder = "data/{}/".format(archive)
     # the output folder to save trained model in
-    logdir = '{}/{}/ngrams{}-size{}-window{}-mincount{}-negative{}-lr{}/'.format(args.archive,
+    logdir = '{}/{}/year{}-ngrams{}-size{}-window{}-mincount{}-negative{}-lr{}/'.format(args.archive,
                                                                              'cbow' if args.model == 'cbow' else 'SGNS',
+                                                                             args.year,
                                                                              args.wordNgrams,
                                                                              # 'cbow' if args.model == 'cbow' else 'SGNS',
                                                                              args.dim, args.ws, args.minCount,
@@ -34,18 +37,36 @@ if __name__ == '__main__':
                                                                              args.lr)
 
     mkdir(logdir)
-    for input_file in os.listdir(data_folder):
-        year = input_file.split('.')[0]
-        print('year is: {}\n'.format(year))
-        logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-        model = fasttext.train_unsupervised(input=os.path.join(data_folder, input_file),
-                                    wordNgrams=args.wordNgrams,
-                                    model=args.model,
-                                    lr=args.lr,
-                                    dim=args.dim,
-                                    ws=args.ws,
-                                    neg=args.neg if args.model == 'skipgram' else 0,
-                                    thread=mp.cpu_count())
+    input_file = os.path.join(data_folder, '{}.txt'.format(args.year))
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-        model.save_model(os.path.join(logdir, "{}.bin".format(year)))
+    print('\nTraining fasttext on {} archive for year {}]\n'.format(args.archive, args.year))
+    model = fasttext.train_unsupervised(input=os.path.join(data_folder, input_file),
+                                        wordNgrams=args.wordNgrams,
+                                        model=args.model,
+                                        lr=args.lr,
+                                        dim=args.dim,
+                                        ws=args.ws,
+                                        neg=args.neg if args.model == 'skipgram' else 0,
+                                        thread=mp.cpu_count())
+
+    model.save_model(os.path.join(logdir, "{}.bin".format(args.year)))
+
+
+    # for input_file in os.listdir(data_folder):
+    #     year = input_file.split('.')[0]
+    #     print('year is: {}\n'.format(year))
+    #     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+    #     model = fasttext.train_unsupervised(input=os.path.join(data_folder, input_file),
+    #                                 wordNgrams=args.wordNgrams,
+    #                                 model=args.model,
+    #                                 lr=args.lr,
+    #                                 dim=args.dim,
+    #                                 ws=args.ws,
+    #                                 neg=args.neg if args.model == 'skipgram' else 0,
+    #                                 thread=mp.cpu_count())
+    #
+    #     model.save_model(os.path.join(logdir, "{}.bin".format(year)))
+
+
 
