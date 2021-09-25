16b2810a849e1eaac351cb55d76e08fdafc935a6
Hiyam Ghannam <hkg02@mail.aub.edu>
2021-09-25 14:27:55 +0300

    added usage_change and alignment for fasttext

---
+++ semantic_shifts/usage_change/source/run.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# if you want to use the standard datasets, 
+# set this variable to the extracted data and embeddings from
+# https://1drv.ms/u/s!AlflMXNPVy-wgYwvnHTl-CfOoQh_qw?e=Q8hptH
+STD_DIR=/home/ganeshjw/projects/rrg-mageed/ganeshjw/objects/usage_change
+
+# set this variable to the result directory
+RES_DIR=/home/ganeshjw/projects/rrg-mageed/ganeshjw/objects/usage_change/temp
+
+# standard dataset configuration
+declare -A data
+data=( ["young"]="birthyear.1990_2009.lowercase" ["old"]="birthyear.1950_1969.lowercase" ["male"]="gender.male.lowercase" ["female"]="gender.female.lowercase" ["performer"]="occupation.performer.lowercase" ["sports"]="occupation.sports.lowercase" ["creator"]="occupation.creator.lowercase" ["weekday"]="yang.weekday.lowercase" ["weekend"]="yang.weekend.lowercase" ["french2014"]="french.2014.lowercase" ["french2018"]="french.2018.lowercase" ["hebrew2014"]="hebrew.2014.lowercase" ["hebrew2018"]="hebrew.2018.lowercase")
+
+# train word2vec on custom dataset
+if [ $1 == "train" ] ; then
+  if [ $2 == "custom" ] ; then
+    python source/word2vec.py --data $3 --save $RES_DIR/$4
+    python source/word2vec.py --data $5 --save $RES_DIR/$6
+  fi
+fi
+
+# detect words with usage change
+if [ $1 == "detect" ] ; then
+  if [ $2 == "custom" ] ; then
+    python source/extract_from_single_split.py --data_a $3 --data_b $4 --embed_a $RES_DIR/$5.seed123 --embed_b $RES_DIR/$6.seed123 --name_split_a $5 --name_split_b $6 --out_topk $RES_DIR/detect_$5_$6_ 
+  fi
+  if [ $2 == "standard" ] ; then
+    python source/extract_from_single_split.py --data_a $STD_DIR/tokdata/"${data[$3]}" --data_b $STD_DIR/tokdata/"${data[$4]}" --embed_a $STD_DIR/embeddings/"${data[$3]}".seed123.mfreq20 --embed_b $STD_DIR/embeddings/"${data[$4]}".seed123.mfreq20 --name_split_a $3 --name_split_b $4 --out_topk $RES_DIR/detect_$3_$4_
+  fi
+fi
+
+# visualize neighbors for given words
+if [ $1 == "visualize" ] ; then
+  if [ $2 == "custom" ] ; then
+    python source/visualize_neighbors.py --data_a $3 --data_b $4 --embed_a $RES_DIR/$5.seed123 --embed_b $RES_DIR/$6.seed123 --name_split_a $5 --name_split_b $6 --out_dir $RES_DIR/vis_$5_$6_ --words $7
+  fi
+  if [ $2 == "standard" ] ; then
+    python source/visualize_neighbors.py --data_a $STD_DIR/tokdata/"${data[$3]}" --data_b $STD_DIR/tokdata/"${data[$4]}" --embed_a $STD_DIR/embeddings/"${data[$3]}".seed123.mfreq20 --embed_b $STD_DIR/embeddings/"${data[$4]}".seed123.mfreq20 --name_split_a $3 --name_split_b $4 --out_dir $RES_DIR/vis_$3_$4_ --words $5
+  fi
+fi
+
