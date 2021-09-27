020a57b931ba37b55cfef7248eb424e6ce24e894
hiyamgh <hkg02@mail.aub.edu>
2021-09-27 10:21:03 +0300

    added heuristic to get intersection between bneighbours that accounts for OCR errors

--- semantic_shifts/test.py
+++ semantic_shifts/test.py
@@ -1,17 +1,90 @@
 import fasttext
+from nltk import everygrams
+from nltk import ngrams
+#
+# w1 = 'تقوماسرائيل'
+# w2 = 'توقفاسرائيل'
+
+
+# lst = list(everygrams(w1, 1, 3))
+# for e in lst:
+# #     print(e)
+# N = len(w1)
+# grams = [w1[i:i+N] for i in range(len(w1)-N+1)]
+# for g in grams:
+#     print(g)
+#
+
+
+# ('', 'تقوماسرائيل')
+# ('ت', 'قوماسرائيل')
+# ('تق', 'وماسرائيل')
+# ('تقو', 'ماسرائيل')
+# ('تقوم', 'اسرائيل')
+# ('تقوما', 'سرائيل')
+# ('تقوماس', 'رائيل')
+# ('تقوماسر', 'ائيل')
+# ('تقوماسرا', 'ئيل')
+# ('تقوماسرائ', 'يل')
+# ('تقوماسرائي', 'ل')
+# -----------------------------
+# ('', 'توقفاسرائيل')
+# ('ت', 'وقفاسرائيل')
+# ('تو', 'قفاسرائيل')
+# ('توق', 'فاسرائيل')
+# ('توقف', 'اسرائيل')
+# ('توقفا', 'سرائيل')
+# ('توقفاس', 'رائيل')
+# ('توقفاسر', 'ائيل')
+# ('توقفاسرا', 'ئيل')
+# ('توقفاسرائ', 'يل')
+# ('توقفاسرائي', 'ل')
+
+# for i in range(len(w1)):
+#     print((w1[0:i], w1[i:]))
+#
+# print('-----------------------------')
+#
+# for i in range(len(w2)):
+#     print((w2[0:i], w2[i:]))
+#
+# print((len(w1) - len('اسرائيل'))/ max(len('اسرائيل'), len(w1)))
 
 model1 = fasttext.load_model('../Train_Word_Embedidng/fasttext/nahar/SGNS/ngrams4-size300-window5-mincount100-negative15-lr0.001/2006.bin')
 model2 = fasttext.load_model('../Train_Word_Embedidng/fasttext/assafir/SGNS/ngrams4-size300-window5-mincount100-negative15-lr0.001/2006.bin')
-
+#
 w = 'اسرائيل'
 neighs1 = set(out[1] for out in model1.get_nearest_neighbors(w, 1000))
 neighs2 = set(out[1] for out in model2.get_nearest_neighbors(w, 1000))
-
-common = []
+#
+common = set()
 for nn1 in neighs1:
     for nn2 in neighs2:
         maxlen = max(len(nn1), len(nn2))
-        # if nn1 in nn2 or nn2 in nn1:
+        # if there is a huge intersection between the characheters of both words
         if maxlen - len(''.join(set(nn1).intersection(nn2))) <= 2:
-           common.append((nn1, nn2))
-           print((nn1, nn2))
+            ngrams_nn1, ngrams_nn2 = [], []
+            for i in range(len(nn1)):
+                ngrams_nn1.append(nn1[0:i])
+                ngrams_nn1.append(nn1[i:])
+            for i in range(len(nn2)):
+                ngrams_nn2.append(nn2[0:i])
+                ngrams_nn2.append(nn2[i:])
+            # get the intersection
+            cmn = set(ngrams_nn1).intersection(set(ngrams_nn2))
+            # sort by decreasing order of length
+            cmn_sorted = sorted(list(cmn), key=lambda x: (-len(x), x))
+            # get original word for comparison
+            original = nn1 if len(nn1) >= len(nn2) else nn2
+            # if there exist a word in the intersection that is less than the original word by 40% max, then add it
+            diffs = {}
+            for cw in cmn:
+                if abs((len(original) - len(cw))) / len(original) <= 0.4:
+                    # common.add(nn1 if len(nn1) >= len(nn2) else nn2)
+                    common.add(cw)
+                    print('{} is subset of {}, added {}'.format(cw, original, (nn1, nn2)))
+                    break
+
+print('============================================================================================')
+print(len(common))
+print(common)
---
+++ semantic_shifts/test_fasttext.py
@@ -0,0 +1,240 @@
+import fasttext
+import argparse
+import operator
+from collections import defaultdict
+from tqdm import tqdm
+import codecs
+import pickle
+
+
+
+def load_and_normalize(lang, filename, vocab, wv, w2i, hamilton=False):
+    # load word embeddings, vocab file and update the global maps (vocab, wv, w2i)
+
+    modela = fasttext.load_model(filename)
+    modelb = fasttext.load_model(filename)
+
+    # get vocabulary
+
+    # # load word embeddings, vocab file
+    # if hamilton:
+    #     vocab_muse, wv_muse, w2i_muse = load_embeddings_hamilton(filename)
+    # else:
+    #     vocab_muse, wv_muse, w2i_muse = load_embeddings_from_np(filename)
+    #
+    # # normalize the word embeddings
+    # wv_muse = normalize(wv_muse)
+    #
+    # # update the global maps
+    # vocab[lang] = vocab_muse
+    # wv[lang] = wv_muse
+    # w2i[lang] = w2i_muse
+    # print('loaded and normalized %s embeddings'%filename)
+
+
+def load_all_embeddings(args):
+    vocab = {}
+    wv = {}
+    w2i = {}
+    load_and_normalize(args.name_split_a, args.embed_a, vocab, wv, w2i)
+    load_and_normalize(args.name_split_b, args.embed_b, vocab, wv, w2i)
+    return vocab, wv, w2i
+
+
+def extract_freqs(filename, vocab):
+    ''' extract word raw frequencies and normalized frequencies '''
+    print('extracting freqs %s' % filename)
+    count = defaultdict(int)
+    with open(filename, 'r') as f:
+        for l in f:
+            for w in l.strip().split():
+                count[w] += 1
+
+    # consider only words in the vocabulary
+    count_vocab = defaultdict(int)
+    for w in vocab:
+        if w in count:
+            count_vocab[w] = count[w]
+
+    # normalized frequencies
+    tot = sum([count_vocab[item] for item in count_vocab])
+    freq_norm = defaultdict(int)
+    for w in count_vocab:
+        freq_norm[w] = count_vocab[w] / float(tot)
+
+    # top-frequent
+    top_freq = defaultdict(int)
+    sorted_words = [x[0] for x in sorted(count_vocab.items(), key=operator.itemgetter(1))]
+    cutoff = len(sorted_words) / float(20)
+    top_freq_words = sorted_words[int(4 * cutoff):-200]  # -int(cutoff)]
+    for w in top_freq_words:
+        top_freq[w] = count[w]
+
+    print('done')
+    return freq_norm, count_vocab, top_freq
+
+
+# def topK(w, space, k=10, count = None, min_freq = 0):
+#     # identify the top k neighbors of a word in a space
+#
+#     # extract the word vector for word w
+#     idx = w2i[space][w]
+#     vec = wv[space][idx, :]
+#
+#     # compute similarity of w with all words in the vocabulary
+#     sim = wv[space].dot(vec)
+#
+#     # sort similarities by descending order
+#     sort_sim = (sim.argsort())[::-1]
+#
+#     # choose topK
+#     if count:
+#         # consider only the neighbors whose raw frequency is greater than min_freq
+#         best = []
+#         for i in sort_sim:
+#             if i != idx and count[vocab[space][i]] > min_freq:
+#                 best.append(i)
+#                 if len(best) == k:
+#                     break
+#     else:
+#         best = sort_sim[:(k + 1)]
+#
+#     return [vocab[space][i] for i in best if i != idx]
+
+def get_intersection_with_ocr_errors(neighs1, neighs2):
+    common = set()
+    for nn1 in neighs1:
+        for nn2 in neighs2:
+            maxlen = max(len(nn1), len(nn2))
+            # if there is a huge intersection between the characters of both words
+            if maxlen - len(''.join(set(nn1).intersection(nn2))) <= 2:
+                ngrams_nn1, ngrams_nn2 = [], []
+                for i in range(len(nn1)):
+                    ngrams_nn1.append(nn1[0:i])
+                    ngrams_nn1.append(nn1[i:])
+                for i in range(len(nn2)):
+                    ngrams_nn2.append(nn2[0:i])
+                    ngrams_nn2.append(nn2[i:])
+                # get the intersection
+                cmn = set(ngrams_nn1).intersection(set(ngrams_nn2))
+                # sort by decreasing order of length
+                cmn_sorted = sorted(list(cmn), key=lambda x: (-len(x), x))
+                # get original word for comparison
+                original = nn1 if len(nn1) >= len(nn2) else nn2
+                # if there exist a word in the intersection that is less than the original word by 40% max, then add it
+                for cw in cmn:
+                    if abs((len(original) - len(cw))) / len(original) <= 0.4:
+                        # common.add(nn1 if len(nn1) >= len(nn2) else nn2)
+                        common.add(cw)
+                        print('{} is subset of {}, added {}'.format(cw, original, (nn1, nn2)))
+                        break
+    return common
+
+
+def NN_scores():
+    # rank all the words based on our method
+
+    # compute nearest neighbors overlap for all the common words
+    nn_scores = []
+    pbar = tqdm(total=len(vocab[val1]))
+    for i, w in enumerate(vocab[val1]):
+        # if w not in s_words and w in freq1 and w in freq2 and count1[w] > MIN_COUNT and count2[w] > MIN_COUNT:
+        # if w in freq1 and w in freq2 and count1[w] > MIN_COUNT and count2[w] > MIN_COUNT:
+            # neighbors_bef = set(topK(w, space1, args.k, count1, 100))
+            # neighbors_aft = set(topK(w, sapce2, args.k, count2, 100))
+
+        if w in vocab[val1] and w in vocab[val2]:
+            neighbors_bef = set(out[1] for out in model1.get_nearest_neighbors(w, args.k))
+            neighbors_aft = set(out[1] for out in model2.get_nearest_neighbors(w, args.k))
+            common = get_intersection_with_ocr_errors(neighs1=neighbors_bef, neighs2=neighbors_aft)
+            nn_scores.append((len(common), w))
+
+        if i % 10 == 0:
+            pbar.update(10)
+    pbar.close()
+    print('len of ranking', len(nn_scores))
+
+    # rank these words
+    nn_scores_sorted = sorted(nn_scores)
+    for t in nn_scores[:50]:
+        print(t)
+
+    with open('nn_scores_{}_{}.pkl'.format(val1, val2), 'wb') as f:
+        pickle.dump(nn_scores_sorted, f)
+    return nn_scores_sorted
+
+
+# def topK(w, space, k=10, count = None, min_freq = 0):
+#     # identify the top k neighbors of a word in a space
+#
+#     # extract the word vector for word w
+#     idx = w2i[space][w]
+#     vec = wv[space][idx, :]
+#
+#     # compute similarity of w with all words in the vocabulary
+#     sim = wv[space].dot(vec)
+#
+#     # sort similarities by descending order
+#     sort_sim = (sim.argsort())[::-1]
+#
+#     # choose topK
+#     if count:
+#         # consider only the neighbors whose raw frequency is greater than min_freq
+#         best = []
+#         for i in sort_sim:
+#             if i != idx and count[vocab[space][i]] > min_freq:
+#                 best.append(i)
+#                 if len(best) == k:
+#                     break
+#     else:
+#         best = sort_sim[:(k + 1)]
+#
+#     return [vocab[space][i] for i in best if i != idx]
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--embed_a",
+                        default='../Train_Word_Embedidng/fasttext/nahar/SGNS/'
+                                'ngrams4-size300-window5-mincount100-negative15-lr0.001/2006.bin',
+                        help="prefix for embedding for split a")
+    parser.add_argument("--embed_b",
+                        default='../Train_Word_Embedidng/fasttext/assafir/SGNS/'
+                                'ngrams4-size300-window5-mincount100-negative15-lr0.001/2006.bin',
+                        help="prefix for embedding for split b")
+    parser.add_argument("--data_a",
+                        default='../Train_Word_Embedidng/fasttext/data/nahar/2006.txt',
+                        help="name of tokenized data file for split a")
+    parser.add_argument("--data_b",
+                        default='../Train_Word_Embedidng/fasttext/data/assafir/2006.txt',
+                        help="name of tokenized data file for split b")
+    parser.add_argument("--name_split_a",
+                        default='nahar_2006',
+                        help="short name for split a")
+    parser.add_argument("--name_split_b",
+                        default='assafir_2006',
+                        help="short name for split b")
+    parser.add_argument("--out_topk",
+                        default='/tmp/result',
+                        help="prefix for the output files for topk and latex table")
+    parser.add_argument("--freq_thr", type=float, default=0.00001, help="frequency threshold")
+    parser.add_argument("--min_count", type=int, default=200, help="min appearances of a word")
+    parser.add_argument("--k", type=int, default=10000, help="k of k-NN to use")
+
+    args = parser.parse_args()
+
+    model1 = fasttext.load_model(args.embed_a)
+    model2 = fasttext.load_model(args.embed_b)
+
+    MIN_COUNT = args.min_count
+
+    val1, val2 = args.name_split_a, args.name_split_b
+
+    vocab = {}
+    vocab[val1] = model1.words
+    vocab[val2] = model2.words
+
+    # freq_norm_val1, count_vocab_val1, top_freq_val1 = extract_freqs(args.data_a, vocab[val1])
+    # freq_norm_val2, count_vocab_val2, top_freq_val2 = extract_freqs(args.data_b, vocab[val2])
+
+    NN_scores()
