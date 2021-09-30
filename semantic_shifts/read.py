0bf01a29a8a4b18742f13d6e47242b110c9a797f
Hiyam G <hkg02@mail.aub.edu>
2021-09-30 06:49:38 +0300

    repo cleanup

--- semantic_shifts/read.py
+++
@@ -1,8 +0,0 @@
-import pickle
-import os
-
-for year in range(1934, 2006):
-    with open('D:/results_deltas/{}-disps.pkl'.format(year), 'rb') as handlle:
-        result = pickle.load(handlle)
-
-    print('{}: {}'.format(year, result))
