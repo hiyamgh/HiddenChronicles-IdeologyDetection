43ca31bf176863c2191a7e2d46dd6c44e875c9a3
hiyamgh <hkg02@mail.aub.edu>
2021-11-08 20:09:54 +0200

    added recent code for mining for categories and saving to excel sheets

--- semantic_shifts/wikipedia/search.py
+++ semantic_shifts/wikipedia/search.py
@@ -6,17 +6,23 @@ import json
 
 S = requests.Session()
 
-res = wikipedia.search("Lebanon War", results=4, suggestion=False)
+res = wikipedia.search("Lebanon War", results=100, suggestion=False)
 all_categories = {}
 all_categories_set = set()
 count = 1
-df = pd.DataFrame()
 for a in res:
-    article_page = wikipedia.page(a)
-    print("{}: article title: {}, url: {}\nCategories: {}".format(count, article_page.title, article_page.url, article_page.categories))
-    all_categories[article_page.title] = article_page.categories
-    for c in article_page.categories:
-        all_categories_set.add(c)
+    try:
+        article_page = wikipedia.page(a)
+        print("{}: article title: {}, url: {}\nCategories: {}".format(count, article_page.title, article_page.url, article_page.categories))
+        all_categories[article_page.title] = article_page.categories
+        for c in article_page.categories:
+            if 'Articles containing' not in c:
+                all_categories_set.add(c)
+    except wikipedia.exceptions.PageError:
+        # if a "PageError" was raised, ignore it and continue to next link
+        continue
+    except wikipedia.DisambiguationError as e:
+        continue
     count += 1
 
 with open('categories.pickle', 'wb') as handle:
@@ -30,6 +36,9 @@ URL = "https://en.wikipedia.org/w/api.php"
 
 max_times = 5
 times = 0
+cats_with_no_subcats = []
+len_mod_300 = 1
+df = pd.DataFrame(columns=['category', 'sub-category'])
 # now, for each category encountered, get all its sub-categories
 for c in all_categories_set:
     print('Processing Category: {}'.format(c))
@@ -47,9 +56,41 @@ for c in all_categories_set:
         try:
             R = S.get(url=URL, params=PARAMS)
             DATA = R.json()
-            print('number of sub-categories: {}'.format(len(DATA["query"]["categorymembers"])))
-            with open('{}-subcategories.json'.format(c), 'w', encoding='utf-8') as f:
-                json.dump(DATA, f, ensure_ascii=False, indent=4)
+            length = len(DATA["query"]["categorymembers"])
+            print('number of sub-categories: {}'.format(length))
+            if length > 0:
+                subcats = DATA["query"]["categorymembers"]
+                count = 0
+                for sub in subcats:
+                    if count == 0:
+                        df = df.append({
+                            'category': c,
+                            'sub-category': sub['title']
+                        }, ignore_index=True)
+                    else:
+                        df = df.append({
+                            'category': '',
+                            'sub-category': sub['title']
+                        }, ignore_index=True)
+
+                    count += 1
+
+                # with open('{}-subcategories.json'.format(c), 'w', encoding='utf-8') as f:
+                #     json.dump(DATA, f, ensure_ascii=False, indent=4)
+            else:
+                df = df.append({
+                    'category': c,
+                    'sub-category': ''
+                }, ignore_index=True)
+
+                print('will not save \'{}\' because it has no subcategories'.format(c))
+                cats_with_no_subcats.append(c)
+                pass
+
+            if len(df) % 300 == 0:
+                df.to_csv('categories-batch-{}.csv'.format(len_mod_300), index=False)
+                len_mod_300 += 1
+                df = pd.DataFrame(columns=['category', 'sub-category'])
             success = 1
             break
         except requests.exceptions.ConnectionError:
@@ -59,3 +100,9 @@ for c in all_categories_set:
         success = 0
     else:
         print("\'{w}\' failed too many times ({t}) times. " + "Moving on".format(w=c, t=times))
+
+df.to_csv('categories-batch-{}.csv'.format(len_mod_300), index=False)
+
+# save list of all categories that have no subcategories:
+with open("all_cats_with_no_subcats.txt", "wb") as fp:
+    pickle.dump(cats_with_no_subcats, fp)
