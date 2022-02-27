3cc77af7d7d36a0c0737fcc3a407eab89e622ad7
hiyamgh <hkg02@mail.aub.edu>
2022-02-27 13:39:49 +0200

    added latest codes to gather summaries

--- semantic_shifts/words_are_malleable_stability/summaries_new/manual/check2.py
+++ semantic_shifts/words_are_malleable_stability/summaries_new/manual/check2.py
@@ -1,10 +1,25 @@
 import os
 
-with open('temp.txt', 'r', encoding='utf-8') as f:
+with open('nahar/summaries_azarbonyad.txt', 'r', encoding='utf-8') as f:
+    sofar = set()
     alllines = f.readlines()
     for line in alllines:
         if 'please' not in line:
-            line = line[:-1]
-            line = ''.join([c for c in line if not c.isdigit() and c != ':'])
-            # print('\'\':\'{}\','.format(line[:-1]))
-            print('\'{}\':\'\','.format(line[:-1]))
+            line = ''.join([c for c in line if not c.isdigit() and c != ':' and c != '\n' and c != '\t'])
+            if line.strip() != '' and line.strip() not in sofar and line.strip() not in ['saudiya', 'hariri', 'munazama', 'hezbollah', 'iran', 'falastini', 'syrian', 'arafat', 'mukawama']:
+                print('\'{}\':\'\','.format(line.strip()))
+                sofar.add(line.strip())
+            # print('\'{}\':\'\','.format(line[:-1]))
+
+    with open('assafir/summaries_azarbonyad.txt', 'r', encoding='utf-8') as f1:
+        alllines = f1.readlines()
+        for line in alllines:
+            if 'please' not in line:
+                line = ''.join([c for c in line if not c.isdigit() and c != ':' and c != '\n' and c != '\t'])
+                if line.strip() != '' and line.strip() not in sofar and line.strip() not in ['saudiya', 'hariri',
+                                                                                             'munazama', 'hezbollah',
+                                                                                             'iran', 'falastini',
+                                                                                             'syrian', 'arafat',
+                                                                                             'mukawama']:
+                    print('\'{}\':\'\','.format(line.strip()))
+                    sofar.add(line.strip())
