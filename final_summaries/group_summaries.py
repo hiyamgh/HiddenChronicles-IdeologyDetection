f2273988be727ed4ed8f2bd179bb2b7d78ee832a
hiyamgh <hkg02@mail.aub.edu>
2022-02-28 10:49:41 +0200

    added latest codes

--- final_summaries/group_summaries.py
+++ final_summaries/group_summaries.py
@@ -6,18 +6,19 @@ with open('../semantic_shifts/words_are_malleable_stability/summaries_new/manual
     for l in all_lines:
         if 'please' in l:
             year = l.split(':')[2][:-1] # get the year
+            year = year.strip()
             if year == '1985':
                 print()
-            if year not in years2words:
-                years2words[year] = {}
+
             wordinside = l.split(':')[1]
             idx = wordinside.index('in year')
             word = wordinside[:idx] # get the word
-            if word not in years2words[year]:
-                years2words[year][word] = []
-            else:
-                word = word + '_1'
-                years2words[year][word] = []
+
+            if word not in years2words:
+                years2words[word] = {}
+
+            if year not in years2words[word]:
+                years2words[word][year] = []
         else:
             if l == '\n':
                 continue
@@ -25,7 +26,7 @@ with open('../semantic_shifts/words_are_malleable_stability/summaries_new/manual
                 continue
             w_curr = l.strip()
             w_curr = w_curr[:-1]
-            years2words[year][word].append(w_curr)
+            years2words[word][year].append(w_curr)
 
 with open('year2word2summary_nahar.pickle', 'wb') as handle:
     pickle.dump(years2words, handle, protocol=pickle.HIGHEST_PROTOCOL)
@@ -35,19 +36,20 @@ with open('../semantic_shifts/words_are_malleable_stability/summaries_new/manual
     all_lines = f.readlines()
     for l in all_lines:
         if 'please' in l:
-            year = l.split(':')[2][:-1] # get the year
+            year = l.split(':')[2][:-1]  # get the year
+            year = year.strip()
             if year == '1985':
                 print()
-            if year not in years2words:
-                years2words[year] = {}
+
             wordinside = l.split(':')[1]
             idx = wordinside.index('in year')
-            word = wordinside[:idx] # get the word
-            if word not in years2words[year]:
-                years2words[year][word] = []
-            else:
-                word = word + '_1'
-                years2words[year][word] = []
+            word = wordinside[:idx]  # get the word
+
+            if word not in years2words:
+                years2words[word] = {}
+
+            if year not in years2words[word]:
+                years2words[word][year] = []
         else:
             if l == '\n':
                 continue
@@ -55,7 +57,7 @@ with open('../semantic_shifts/words_are_malleable_stability/summaries_new/manual
                 continue
             w_curr = l.strip()
             w_curr = w_curr[:-1]
-            years2words[year][word].append(w_curr)
+            years2words[word][year].append(w_curr)
 
 with open('year2word2summary_assafir.pickle', 'wb') as handle:
     pickle.dump(years2words, handle, protocol=pickle.HIGHEST_PROTOCOL)
--- final_summaries/trace_emotions_sentiment.py
+++ final_summaries/trace_emotions_sentiment.py
@@ -1,45 +1,84 @@
-# import alyahmor.genelex
-# import naftawayh.wordtag
 import pickle
 import qalsadi.lemmatizer
 import numpy as np
 import matplotlib.pyplot as plt
 import os
 from manual_corrections_azarbonyad import azarbonyad
-# import farasa
-# from farasa.stemmer import FarasaStemmer
+
+lemmer = qalsadi.lemmatizer.Lemmatizer()
+
+
+def collect_summaries(stored_summaries, emotions_sentiment_ar, years):
+    summaries = {}
+
+    for w in stored_summaries:
+        summaries[w] = {}
+        for year in stored_summaries[w]:
+            if year in years: # if we want that year
+                summaries[w][year] = []
+                summary = stored_summaries[w][year]
+                for s in summary:
+                    if s in emotions_sentiment_ar:
+                        summaries[w][year].append(s)
+                    else:
+                        if s in azarbonyad:
+                            s = azarbonyad[s] if azarbonyad[s] != '' and azarbonyad[s] != '0' else s
+
+                        if ' ' in s:
+                            for sp in s.split(' '):
+                                if sp in emotions_sentiment_ar:
+                                    summaries[w][year].append(sp)
+                                else:
+                                    sp = lemmer.lemmatize(sp)
+                                    if sp in emotions_sentiment_ar:
+                                        summaries[w][year].append(sp)
+                        else:
+                            if s in emotions_sentiment_ar:
+                                summaries[w][year].append(s)
+                            else:
+                                sp = lemmer.lemmatize(s)
+                                if sp in emotions_sentiment_ar:
+                                    summaries[w][year].append(sp)
+
+                print('processed year: {}'.format(year))
+
+    return summaries
+
+
+def get_scores(summaries, emotions_aggregated):
+    scores = {}
+    for w in summaries:
+        for em in emotions_aggregated:
+            for year in summaries[w]:
+                if w not in scores:
+                    scores[w] = {}
+                if year not in scores[w]:
+                    scores[w][year] = {}
+
+                for s in summaries[w][year]:
+                    for sub_em in emotions_aggregated[em]:
+                        if em not in scores[w][year]:
+                            scores[w][year][em] = []
+                        # scores[w][em][year] = []
+                        scores[w][year][em].append(emotions_sentiment_ar[s][sub_em])
+    return scores
+
 
 # load the dictionary of emotions
 with open('../semantic_shifts/words_are_malleable_stability/ArSEL_ArSenL_database/emotions_sentiment_ar.pkl', 'rb') as f:
     emotions_sentiment_ar = pickle.load(f)
 
-# define lemmatizer thats actually a stemmer
-lemmer = qalsadi.lemmatizer.Lemmatizer()
-# stemmer = FarasaStemmer()
-# tagger = naftawayh.wordtag.WordTagger()
-
-years = [1982, 1983, 1984, 1996, 1997, 1998, 2000, 2005, 2006, 2007, 2008, 2009, 2010, 2011]
 with open('year2word2summary_nahar.pickle', 'rb') as handle:
     nahar = pickle.load(handle)
 
-    summaries = {}
-    for year in years:
-        summaries[year] = {}
-        for w in nahar[year]:
-            summaries[year][w] = []
-            summary = nahar[year][w]
-            for s in summary:
-                if s in emotions_sentiment_ar:
-                    summaries[year][w].append(s)
-                else:
-                    if ' ' in s:
-                        splitted = s.split(' ')
-                        for sp in splitted:
-                            if sp in azarbonyad:
-                                sp = azarbonyad[sp]
-                                if ' ' in sp:
-                                    for spp in sp.split(' '):
-                                        if
+with open('year2word2summary_assafir.pickle', 'rb') as handle:
+    assafir = pickle.load(handle)
+
+years_nahar = ['1982', '1983', '1984', '1996', '1997', '1998', '2000', '2005', '2006', '2007', '2008', '2009']
+years_assafir = ['1982', '1983', '1984', '1996', '1997', '1998', '2000', '2005', '2006', '2007', '2008', '2009', '2010', '2011']
+
+summaries_nahar = collect_summaries(nahar, emotions_sentiment_ar, years_nahar)
+summaries_assafir = collect_summaries(assafir, emotions_sentiment_ar, years_assafir)
 
 
 # loop over summaries and store the ones found in the emotion lexicon
@@ -113,45 +152,61 @@ emotions_aggregated = {
     'afraid': ['AFRAID'],
     'sad': ['SAD']
 }
-scores = {}
-for w in summaries:
-    for em in emotions_aggregated:
-        for year in summaries[w]:
-            if w not in scores:
-                scores[w] = {}
-            if year not in scores[w]:
-                scores[w][year] = {}
+# scores = {}
+# for w in summaries:
+#     for em in emotions_aggregated:
+#         for year in summaries[w]:
+#             if w not in scores:
+#                 scores[w] = {}
+#             if year not in scores[w]:
+#                 scores[w][year] = {}
+#
+#             for s in summaries[w][year]:
+#                 for sub_em in emotions_aggregated[em]:
+#                     if em not in scores[w][year]:
+#                         scores[w][year][em] = []
+#                     # scores[w][em][year] = []
+#                     scores[w][year][em].append(emotions_sentiment_ar[s][sub_em])
+# print(scores)
 
-            for s in summaries[w][year]:
-                for sub_em in emotions_aggregated[em]:
-                    if em not in scores[w][year]:
-                        scores[w][year][em] = []
-                    # scores[w][em][year] = []
-                    scores[w][year][em].append(emotions_sentiment_ar[s][sub_em])
-print(scores)
+scores_nahar = get_scores(summaries_nahar, emotions_aggregated)
+scores_assafir = get_scores(summaries_assafir, emotions_aggregated)
 
 # loop over scores and get avg values per word per year
-avg_scores_yearly = {}
-for w in scores:
-    avg_scores_yearly[w] = {}
-    years = [y for y in scores[w]]
-    for y in years:
-        avg_scores_yearly[w][y] = {}
-        for em in emotions_aggregated:
-            avg = np.mean(scores[w][y][em]) if em in scores[w][y] else 0
-            avg_scores_yearly[w][y][em] = avg
-print(avg_scores_yearly)
+avg_scores_yearly_nahar = {}
+avg_scores_yearly_assafir = {}
 
-std_scores_yearly = {}
-for w in scores:
-    std_scores_yearly[w] = {}
-    years = [y for y in scores[w]]
+for w in scores_nahar:
+    avg_scores_yearly_nahar[w] = {}
+    years = [y for y in scores_nahar[w]]
     for y in years:
-        std_scores_yearly[w][y] = {}
+        avg_scores_yearly_nahar[w][y] = {}
         for em in emotions_aggregated:
-            avg = np.std(scores[w][y][em]) if em in scores[w][y] else 0
-            std_scores_yearly[w][y][em] = avg
-print(std_scores_yearly)
+            avg = np.mean(scores_nahar[w][y][em]) if em in scores_nahar[w][y] else 0
+            avg_scores_yearly_nahar[w][y][em] = avg
+print(avg_scores_yearly_nahar)
+
+for w in scores_assafir:
+    avg_scores_yearly_assafir[w] = {}
+    years = [y for y in scores_assafir[w]]
+    for y in years:
+        avg_scores_yearly_assafir[w][y] = {}
+        for em in emotions_aggregated:
+            avg = np.mean(scores_assafir[w][y][em]) if em in scores_assafir[w][y] else 0
+            avg_scores_yearly_assafir[w][y][em] = avg
+print(avg_scores_yearly_assafir)
+
+# std_scores_yearly = {}
+# for w in scores:
+#     std_scores_yearly[w] = {}
+#     years = [y for y in scores[w]]
+#     for y in years:
+#         std_scores_yearly[w][y] = {}
+#         for em in emotions_aggregated:
+#             avg = np.std(scores[w][y][em]) if em in scores[w][y] else 0
+#             std_scores_yearly[w][y][em] = avg
+# print(std_scores_yearly)
+#
 
 
 def mkdir(folder):
@@ -160,7 +215,7 @@ def mkdir(folder):
 
 
 # line plots
-plots_folder_lines = 'plots/line_plots/'
+plots_folder_lines = 'plots/line_plots/nahar/'
 plots_folder_bars = 'plots/bar_plots/'
 
 mkdir(plots_folder_lines)
@@ -173,10 +228,10 @@ def slope(x1, y1, x2, y2):
 
 
 # line plots
-for w in avg_scores_yearly:
-    years = [y for y in avg_scores_yearly[w]]
+for w in avg_scores_yearly_nahar:
+    years = [y for y in avg_scores_yearly_nahar[w]]
     for em in emotions_aggregated:
-        em_over_years = [avg_scores_yearly[w][year][em] for year in years]
+        em_over_years = [avg_scores_yearly_nahar[w][year][em] for year in years]
         if w == 'arafat':
             points = [(i, v) for i, v in enumerate(em_over_years)]
             slopes = []
@@ -192,40 +247,31 @@ for w in avg_scores_yearly:
            ncol=4, fancybox=True, shadow=True)
     fig = plt.gcf()
     fig.set_size_inches(10, 5)
-    plt.savefig(os.path.join(plots_folder_lines, '{}.png'.format(w)))
+    plt.savefig(os.path.join(plots_folder_lines, 'nahar_{}.png'.format(w)))
     plt.close()
 
-# stacked bar plots
 
-# width = 0.35
-for w in avg_scores_yearly:
-    fig, ax = plt.subplots()
-    years = [y for y in avg_scores_yearly[w]]
-    i = 0
-    for em in emotions_aggregated:
-        em_over_years = [avg_scores_yearly[w][year][em] for year in years]
-        if i == 0:
-            ax.bar(years, em_over_years, label=em)
-            em_over_years_old = np.array(em_over_years)
-        else:
-            ax.bar(years, em_over_years, bottom=em_over_years_old, label=em)
-            em_over_years_old += np.array(em_over_years)
-        i += 1
 
-    plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.05),
-           ncol=4, fancybox=True, shadow=True)
-    # fig = plt.gcf()
-    fig.set_size_inches(10, 5)
-    plt.savefig(os.path.join(plots_folder_bars, '{}.png'.format(w)))
-    plt.close()
 
-# generator = alyahmor.genelex.genelex()
-# word = "كتاب"
-# # noun_forms = generator.generate_forms(word, word_type="noun", vocalized=False)
-# # for form in noun_forms:
-# #     print(form)
-# verb_forms = generator.generate_forms("كتب", word_type="verb", vocalized=False)
-# for v in verb_forms:
-#     print(v)
-
-# noun_affix =generator.generate_affix_list(word_type="noun", vocalized=False)
+# # stacked bar plots
+# # width = 0.35
+# for w in avg_scores_yearly:
+#     fig, ax = plt.subplots()
+#     years = [y for y in avg_scores_yearly[w]]
+#     i = 0
+#     for em in emotions_aggregated:
+#         em_over_years = [avg_scores_yearly[w][year][em] for year in years]
+#         if i == 0:
+#             ax.bar(years, em_over_years, label=em)
+#             em_over_years_old = np.array(em_over_years)
+#         else:
+#             ax.bar(years, em_over_years, bottom=em_over_years_old, label=em)
+#             em_over_years_old += np.array(em_over_years)
+#         i += 1
+#
+#     plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.05),
+#            ncol=4, fancybox=True, shadow=True)
+#     # fig = plt.gcf()
+#     fig.set_size_inches(10, 5)
+#     plt.savefig(os.path.join(plots_folder_bars, '{}.png'.format(w)))
+#     plt.close()
