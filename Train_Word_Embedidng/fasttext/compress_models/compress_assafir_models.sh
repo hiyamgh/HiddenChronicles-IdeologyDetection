247c196e4398ac4585f94eb2a961f6c0cea5abf4
hiyamgh <hkg02@mail.aub.edu>
2022-03-15 14:50:12 +0200

    added bash scripts to compress models

---
+++ Train_Word_Embedidng/fasttext/compress_models/compress_assafir_models.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=cmprs-as
+#SBATCH --account=hkg02
+#SBATCH --partition=normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=hkg02@mail.aub.edu
+
+zip -r assafir.zip assafir/
---
+++ Train_Word_Embedidng/fasttext/compress_models/compress_hayat_models.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=cmprs-ha
+#SBATCH --account=hkg02
+#SBATCH --partition=normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=hkg02@mail.aub.edu
+
+zip -r hayat.zip hayat/
