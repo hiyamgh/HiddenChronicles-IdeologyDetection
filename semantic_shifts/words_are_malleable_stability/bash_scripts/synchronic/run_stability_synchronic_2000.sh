5b4ffc5390336b5c00dbf1c2e61cad59a853dee8
hiyamgh <hkg02@mail.aub.edu>
2022-03-26 13:04:01 +0200

    added latest scripts to take last years in each archive for summaries

--- semantic_shifts/words_are_malleable_stability/bash_scripts/synchronic/run_stability_synchronic_2000.sh
+++ semantic_shifts/words_are_malleable_stability/bash_scripts/synchronic/run_stability_synchronic_2000.sh
@@ -8,11 +8,11 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=hkg02@mail.aub.edu
-#SBATCH --array=1-12%6
 
 module load python/3
 
 k=100
+y=2000
 
 EMB_DIR1=/scratch/7613491_hkg02/political_discourse_mining_hiyam/Train_Word_Embedidng/fasttext/nahar/SGNS/ngrams4-size300-window5-mincount100-negative15-lr0.001/
 EMB_DIR2=/scratch/7613491_hkg02/political_discourse_mining_hiyam/Train_Word_Embedidng/fasttext/assafir/SGNS/ngrams4-size300-window5-mincount100-negative15-lr0.001/
@@ -26,10 +26,6 @@ keywords_file=input/all_keywords3.txt
 savedir=/scratch/7613491_hkg02/political_discourse_mining_hiyam/semantic_shifts_modified/stability_synchronic/
 USCOUNTER=1
 
-for y in {1988..2000}; do
-    if [ $USCOUNTER -eq $SLURM_ARRAY_TASK_ID ]; then
-        echo "synchronic: year $y in: $arch1 vs. $arch2"
-        python words_are_malleable4.py --path1 $EMB_DIR1 --path2 $EMB_DIR2 --path3 $EMB_DIR3 --model1 ${y}.bin --model2 ${y}.bin --model3 ${y}.bin --model1_name ${arch1}_${y} --model2_name ${arch2}_${y} --model3_name ${arch3}_${y} --k ${k} --words_file ${keywords_file} --method=combined --save_dir ${savedir}
-    fi
-    USCOUNTER=$(expr $USCOUNTER + 1)
-done
+
+echo "synchronic: year $y in: $arch1 vs. $arch2"
+python words_are_malleable4.py --path1 $EMB_DIR1 --path2 $EMB_DIR2 --path3 $EMB_DIR3 --model1 ${y}.bin --model2 ${y}.bin --model3 ${y}.bin --model1_name ${arch1}_${y} --model2_name ${arch2}_${y} --model3_name ${arch3}_${y} --k ${k} --words_file ${keywords_file} --method=combined --save_dir ${savedir}
