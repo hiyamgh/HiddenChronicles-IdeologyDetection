b3099401c8d865f24604e47898d223cf40c1e4d8
Hiyam G <hkg02@mail.aub.edu>
2022-03-26 10:04:58 +0200

    refactored scripts for synchronic and diachronic summaries

--- semantic_shifts/words_are_malleable_stability/bash_scripts/diachronic/run_stability_diachronic_hayat.sh
+++ semantic_shifts/words_are_malleable_stability/bash_scripts/diachronic/run_stability_diachronic_hayat.sh
@@ -8,11 +8,11 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=hkg02@mail.aub.edu
-#SBATCH --array=1-11%3
+#SBATCH --array=1-11%2
 
 module load python/3
 
-neighs=(100)
+k=100
 EMB_DIR1=/scratch/7613491_hkg02/political_discourse_mining_hiyam/Train_Word_Embedidng/fasttext/hayat/SGNS/ngrams4-size300-window5-mincount100-negative15-lr0.001/
 arch1=hayat
 arch2=hayat
@@ -21,15 +21,15 @@ savedir=/scratch/7613491_hkg02/political_discourse_mining_hiyam/semantic_shifts_
 USCOUNTER=1
 
 
-for k in ${neighs[@]}; do
-    for y in {1989..2000}; do
-        ycurr=$((y))
-        yprev=$((y - 1))
-        echo "$yprev vs $ycurr in $arch1 and $arch2"
-        if [ $USCOUNTER -eq $SLURM_ARRAY_TASK_ID ]; then
-            python words_are_malleable4.py --path1 $EMB_DIR1 --path2=$EMB_DIR1 --model1 ${yprev}.bin --model2 ${ycurr}.bin --model1_name ${arch1}_${yprev} --model2_name ${arch2}_${ycurr} --k ${k} --words_file ${keywords_file} --method=combined --save_dir ${savedir}
-        fi
-        USCOUNTER=$(expr $USCOUNTER + 1)
-    done
+
+for y in {1989..2000}; do
+    ycurr=$((y))
+    yprev=$((y - 1))
+    echo "$yprev vs $ycurr in $arch1 and $arch2"
+    if [ $USCOUNTER -eq $SLURM_ARRAY_TASK_ID ]; then
+        python words_are_malleable4.py --path1 $EMB_DIR1 --path2=$EMB_DIR1 --model1 ${yprev}.bin --model2 ${ycurr}.bin --model1_name ${arch1}_${yprev} --model2_name ${arch2}_${ycurr} --k ${k} --words_file ${keywords_file} --method=combined --save_dir ${savedir}
+    fi
+    USCOUNTER=$(expr $USCOUNTER + 1)
 done
+
 
--- semantic_shifts/words_are_malleable_stability/bash_scripts/synchronic/run_stability_synchronic.sh
+++ semantic_shifts/words_are_malleable_stability/bash_scripts/synchronic/run_stability_synchronic.sh
@@ -8,28 +8,28 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=hkg02@mail.aub.edu
-#SBATCH --array=1-12%6
+#SBATCH --array=1-18%6
 
 module load python/3
 
-neighs=(100)
-EMB_DIR1=/scratch/7613491_hkg02/political_discourse_mining_hiyam/Train_Word_Embedidng/fasttext/assafir/SGNS/ngrams4-size300-window5-mincount100-negative15-lr0.001/
+k=100
+
+EMB_DIR1=/scratch/7613491_hkg02/political_discourse_mining_hiyam/Train_Word_Embedidng/fasttext/nahar/SGNS/ngrams4-size300-window5-mincount100-negative15-lr0.001/
 EMB_DIR2=/scratch/7613491_hkg02/political_discourse_mining_hiyam/Train_Word_Embedidng/fasttext/assafir/SGNS/ngrams4-size300-window5-mincount100-negative15-lr0.001/
 EMB_DIR3=/scratch/7613491_hkg02/political_discourse_mining_hiyam/Train_Word_Embedidng/fasttext/hayat/SGNS/ngrams4-size300-window5-mincount100-negative15-lr0.001/
 
 arch1=nahar
 arch2=assafir
 arch3=hayat
+
 keywords_file=input/all_keywords3.txt
 savedir=/scratch/7613491_hkg02/political_discourse_mining_hiyam/semantic_shifts_modified/stability_synchronic/
 USCOUNTER=1
 
-for k in ${neighs[@]}; do
-    for y in {1988..2000}; do
-        echo "synchronic: year $y in: $arch1 vs. $arch2 vs. $arch3"
-        if [ $USCOUNTER -eq $SLURM_ARRAY_TASK_ID ]; then
-            python words_are_malleable3.py --path1 $EMB_DIR1 --path2 $EMB_DIR2 --path3 $EMB_DIR3 --model1 ${y}.bin --model2 ${y}.bin --model3 ${y}.bin --model1_name ${arch1}_${y} --model2_name ${arch2}_${y} --model3_name ${arch3}_${y} --k ${k} --words_file ${keywords_file} --method=combined --save_dir ${savedir}
-        fi
-        USCOUNTER=$(expr $USCOUNTER + 1)
-    done
+for y in {1982..2000}; do
+    if [ $USCOUNTER -eq $SLURM_ARRAY_TASK_ID ]; then
+        echo "synchronic: year $y in: $arch1 vs. $arch2"
+        python words_are_malleable4.py --path1 $EMB_DIR1 --path2 $EMB_DIR2 --path3 $EMB_DIR3 --model1 ${y}.bin --model2 ${y}.bin --model3 ${y}.bin --model1_name ${arch1}_${y} --model2_name ${arch2}_${y} --model3_name ${arch3}_${y} --k ${k} --words_file ${keywords_file} --method=combined --save_dir ${savedir}
+    fi
+    USCOUNTER=$(expr $USCOUNTER + 1)
 done
