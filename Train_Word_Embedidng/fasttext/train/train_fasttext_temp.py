04e08814131f8b4c831ddbf4566a47ac439706ad
hiyamgh <hkg02@mail.aub.edu>
2022-06-28 15:43:24 +0300

    refactored fasttext training after setting wordNgrams=1

--- Train_Word_Embedidng/fasttext/train/train_fasttext_temp.py
+++ Train_Word_Embedidng/fasttext/train/train_fasttext_temp.py
@@ -13,23 +13,14 @@ def mkdir(folder):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-a', '--archive', type=str, default='nahar', help="name of the archive to transform")
-    # parser.add_argument('-c', '--minCount', type=int, help='minimal number of word occurrences')
-    # parser.add_argument('-w', '--wordNgrams', type=int, help='max length of word ngram')
-    # parser.add_argument('-m', '--model', type=str, help='type of model - cbow or skipgram')
-    # parser.add_argument('-l', '--lr', type=float, help='learning rate')
-    # parser.add_argument('-d', '--dim', type=int, help='size of word vectors')
-    # parser.add_argument('-s', '--ws', type=int, help='size of the context window')
-    # parser.add_argument('-n', '--neg', type=int, help='number of negatives sampled')
     parser.add_argument('-y', '--year', type=int, help='year to train on') # use if training at the yearly level
-    # parser.add_argument('-sy', '--start_year', type=int, default=None, help='start year to include') # use if training several years to one embedding
-    # parser.add_argument('-ey', '--end_year', type=int, default=None, help='end year to include') # use if tarining several years to one embedding
     args = parser.parse_args()
 
     archive = args.archive
 
     data_folder = "data/{}/".format(archive)
     input_file = '{}.txt'.format(args.year)
-    logdir = 'temp/'
+    logdir = 'trained_models/{}-skipgram-neg5/'.format(archive)
 
     if os.path.exists(os.path.join(data_folder, input_file)):
         print('started training fasttext word embedding on {} archive, year = {}'.format(archive, args.year))
--- Train_Word_Embedidng/fasttext/train/train_temp_nahar.sh
+++ Train_Word_Embedidng/fasttext/train/train_temp_nahar.sh
@@ -7,7 +7,18 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=hkg02@mail.aub.edu
+#SBATCH --array=1-77%5
 
 module load python/3
 
-python train_fasttext_temp.py --archive nahar --year 1982
+
+START=1933
+END=2009
+USCOUNTER=1
+for i in {1933..2009}; do
+   if [ $USCOUNTER -eq $SLURM_ARRAY_TASK_ID ]; then
+    echo $i
+    python train_fasttext_temp.py --archive nahar --year $i
+   fi
+   USCOUNTER=$(expr $USCOUNTER + 1)
+done
