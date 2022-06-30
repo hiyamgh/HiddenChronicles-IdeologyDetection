bab68ec134482e3a2e422a7509bf059ee05c6be5
hiyamgh <hkg02@mail.aub.edu>
2022-06-30 18:17:24 +0300

    added set 2 of hyperparameters for fasttext training

---
+++ Train_Word_Embedidng/fasttext/train/set2/train_assafir_set2.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=fast-as
+#SBATCH --account=hkg02
+#SBATCH --partition=normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=hkg02@mail.aub.edu
+#SBATCH --array=1-38%5
+
+module load python/3
+
+START=1974
+END=2011
+USCOUNTER=1
+for i in {1974..2011}; do
+   if [ $USCOUNTER -eq $SLURM_ARRAY_TASK_ID ]; then
+    echo $i
+    python train_fasttext_set2.py --archive assafir --year $i
+   fi
+   USCOUNTER=$(expr $USCOUNTER + 1)
+done
