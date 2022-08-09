7e6143f18b28bb969e848da21adbbd8858c88fef
Hiyam Ghannam <hkg02@mail.aub.edu>
2022-08-09 07:40:24 +0300

    refactored code for generating contextual snetences for classification

--- get-discourse-datasets/TextClassification/create_contextual_sentences_classification.py
+++ get-discourse-datasets/TextClassification/create_contextual_sentences_classification.py
@@ -9,7 +9,7 @@ def mkdir(folder):
 
 
 parser = argparse.ArgumentParser(description='Creating Testing sets for Discourse Classification')
-parser.add_argument('--path', type=str, default='/scratch/7613491_hkg02/political_discourse_mining_hiyam/Train_Word_Embedidng/fasttext/data/', help='path to data folders')
+parser.add_argument('--path', type=str, default='C:/Users/hkg02/Downloads/nahar_data/', help='path to data folders')
 parser.add_argument('--archive', type=str, default='nahar', help='name of the archive')
 parser.add_argument('--save_dir', type=str, default='testing_datasets_discourse/', help='directory to save dataset in')
 args = parser.parse_args()
@@ -19,37 +19,35 @@ if __name__ == '__main__':
     keywords = ['مقاومه', 'اسرائيل', 'حزب+الله', 'سوري']
     years = [1982, 1985, 1986, 2001, 2002, 2005, 2006, 2007]
 
-    path = os.path.join(args.path, args.archive)
+    # path = os.path.join(args.path, args.archive)
+    path = args.path
     archive = args.archive
     save_dir = args.save_dir
     for year in years:
         df = pd.DataFrame(columns=['Sentence', 'Year', 'Keyword'])
         sentences_sv, years_sv, keywords_sv = [], [], []
         for keyword in keywords:
-            if '+' in keyword:
+            count = 0
+            if count == 0:
                 print('processing for keyword {} in {}-{}'.format(keyword, year, archive))
-                count = 0
-
+            if '+' in keyword:
                 with open(os.path.join(path, '{}.txt'.format(year)), 'r', encoding='utf-8') as f:
                     lines = f.readlines()
                     for line in lines:
                         tokens = line.strip().split(' ')
 
                         keywords_s = keyword.split('+')
-                        if all([k in tokens for k in keywords_s]) or all(
-                                any([k in t for t in tokens]) for k in keywords_s):
+                        if all([k in tokens for k in keywords_s]) or all(any([k in t for t in tokens]) for k in keywords_s):
                             sentences_sv.append(line)
                             years_sv.append(year)
                             keywords_sv.append(keyword)
                             count += 1
 
-                    print('total count of {} in {}-{}: {}/{}, which is {}%'.format(keyword, archive, year, count,
+                print('total count of {} in {}-{}: {}/{}, which is {}%'.format(keyword, archive, year, count,
                                                                                    len(lines),
                                                                                    (count / len(lines)) * 100))
                 f.close()
             else:
-                print('processing for keyword {} in {}-{}'.format(keyword, year, archive))
-                count = 0
                 with open(os.path.join(path, '{}.txt'.format(year)), 'r', encoding='utf-8') as f:
                     lines = f.readlines()
                     for line in lines:
@@ -60,8 +58,7 @@ if __name__ == '__main__':
                             keywords_sv.append(keyword)
                             count += 1
 
-                    print(
-                        'total count of {} in {}-{}: {}/{}, which is {}%'.format(keyword, archive, year, count, (lines),
+                print( 'total count of {} in {}-{}: {}/{}, which is {}%'.format(keyword, archive, year, count, len(lines),
                                                                                  (count / len(lines)) * 100))
                 f.close()
 
