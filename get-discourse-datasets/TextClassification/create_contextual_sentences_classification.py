29473010bad0bd5de5a1b5b529e62fff796ab2ed
Hiyam Ghannam <hkg02@mail.aub.edu>
2022-08-12 11:29:44 +0300

    added latest modifications to ceating contextual sentences for testing

--- get-discourse-datasets/TextClassification/create_contextual_sentences_classification.py
+++ get-discourse-datasets/TextClassification/create_contextual_sentences_classification.py
@@ -10,7 +10,7 @@ def mkdir(folder):
 
 parser = argparse.ArgumentParser(description='Creating Testing sets for Discourse Classification')
 parser.add_argument('--path', type=str, default='/scratch/7613491_hkg02/political_discourse_mining_hiyam/Train_Word_Embedidng/fasttext/data/', help='path to data folders')
-parser.add_argument('--archive', type=str, default='nahar', help='name of the archive')
+parser.add_argument('--archive', type=str, default='assafir', help='name of the archive')
 parser.add_argument('--save_dir', type=str, default='testing_datasets_discourse/', help='directory to save dataset in')
 args = parser.parse_args()
 
@@ -58,8 +58,7 @@ if __name__ == '__main__':
                             keywords_sv.append(keyword)
                             count += 1
 
-                print( 'total count of {} in {}-{}: {}/{}, which is {}%'.format(keyword, archive, year, count, len(lines),
-                                                                                 (count / len(lines)) * 100))
+                print('total count of {} in {}-{}: {}/{}, which is {}%'.format(keyword, archive, year, count, len(lines), (count / len(lines)) * 100))
                 f.close()
 
         df['Sentence'] = sentences_sv
@@ -68,3 +67,19 @@ if __name__ == '__main__':
 
         mkdir(save_dir)
         df.to_excel(os.path.join(save_dir, 'df_test_{}.xlsx'.format(year)), index=False)
+
+    mappingsar2en = {
+        'مقاومه': 'mukawama',
+        'اسرائيل': 'israel',
+        'حزب+الله': 'hezbollah',
+        'سوري': 'syrian'
+    }
+    for file in ['df_test_{}.xlsx'.format(y) for y in [1982, 1985, 1986, 2001, 2005, 2006, 2007]]:
+        df = pd.read_excel(os.path.join(save_dir, file))
+        print('{}: {}'.format(file, list(set(list(df['Keyword'])))))
+        for keyword in mappingsar2en:
+            df = df[df.Keyword == keyword]
+            if len(df) > 1:
+                df.to_excel(os.path.join(save_dir, '{}_{}.xlsx'.format(file[:-5], mappingsar2en[keyword])), index=False)
+            else:
+                print('word {} not found in {}'.format(keyword, file))
