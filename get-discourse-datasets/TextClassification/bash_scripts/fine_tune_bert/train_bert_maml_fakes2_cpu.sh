227b2f333a62e6a63cfd7990d9a61603610f4db5
Hiyam Ghannam <hkg02@mail.aub.edu>
2022-08-23 12:22:45 +0300

    added module load gcc to run scripts on CPU

---
+++ get-discourse-datasets/TextClassification/bash_scripts/fine_tune_bert/train_bert_maml_fakes2_cpu.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=ft-FN2
+#SBATCH --account=hkg02
+#SBATCH --partition=normal
+
+#SBATCH --mem=16000
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=hkg02@mail.aub.edu
+
+module load java/java8
+module load gcc/10.1.0
+module load python/3
+
+
+df_train=input/FAKES/feature_extraction_train_updated_updated.csv
+df_dev=input/FAKES/feature_extraction_dev_updated_updated.csv
+df_test=input/FAKES/feature_extraction_test_updated.csv
+
+python run_classifier_maml.py --train_set $df_train  --dev_set $df_dev --test_set $df_test --N_shot 10 --output_dir bert_output4/FOMAML/FAKES2/
+
