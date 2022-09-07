96ab8c817a5ccd848d5837ddae9627ddc2bfa37b
Hiyam G <hkg02@mail.aub.edu>
2022-09-07 18:57:37 +0300

    added code to transform ptc to multi label

--- get-discourse-datasets/ptc-corpus/preprocess_datasets.py
+++ get-discourse-datasets/ptc-corpus/preprocess_datasets.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from dataset import *
 import os
 from sklearn.model_selection import train_test_split
@@ -14,15 +16,29 @@ def convert_datasets_single_label_multiclass(df_train, df_test, dev_size=0.2, ra
     return df_train, df_test
 
 
-def get_class_percentages(df_train, df_dev):
-    percentages_train = (df_train['label'].value_counts() / len(df_train)) * 100
-    percentages_dev = (df_dev['label'].value_counts() / len(df_dev)) * 100
+# def get_class_percentages(df_train, df_dev):
+#     percentages_train = (df_train['label'].value_counts() / len(df_train)) * 100
+#     percentages_dev = (df_dev['label'].value_counts() / len(df_dev)) * 100
+#
+#     print('\npercentages in training data:\n')
+#     print(percentages_train)
+#
+#     print('\npercentages in dev data:\n')
+#     print(percentages_dev)
 
-    print('\npercentages in training data:\n')
-    print(percentages_train)
 
-    print('\npercentages in dev data:\n')
-    print(percentages_dev)
+def get_class_percentages(df, label_col, mode):
+    percentages = (df[label_col].value_counts() / len(df)) * 100
+    print('\npercentages in {} data:\n'.format(mode))
+    print(percentages)
+    print('shape of df: {}'.format(df.shape))
+
+
+def get_class_counts(df, label_col, mode):
+    counts = df[label_col].value_counts()
+    print('\ncounts in {} data:\n'.format(mode))
+    print(counts)
+    print('shape of df: {}'.format(df.shape))
 
 
 def translate_dataset(df):
@@ -34,46 +50,114 @@ def translate_dataset(df):
 
 
 if __name__ == '__main__':
-    print(os.getcwd())
-    translator = Translator()
-
     train_data_folder = 'train-articles/'
     dev_data_folder = 'dev-articles/'
     labels_path_train = 'train-task-flc-tc.labels'
     labels_path_dev = 'dev-task-flc-tc.labels'
+    translator = Translator()
 
     train_articles, train_ref_articles_id, train_ref_span_starts, train_ref_span_ends, train_labels = load_data(train_data_folder, labels_path_train)
     dev_articles, dev_ref_articles_id, dev_ref_span_starts, dev_ref_span_ends, dev_labels = load_data(dev_data_folder, labels_path_dev)
 
+    t1 = time.time()
     df_train = dataset_to_pandas(articles=train_articles, ref_articles_id=train_ref_articles_id,
                                  ref_span_starts=train_ref_span_starts, ref_span_ends=train_ref_span_ends,
                                  train_gold_labels=train_labels)
+    unique_contexts = list(set(df_train['context']))
+    print(len(unique_contexts)) # 4313
+    print(len(df_train)) # 6128
 
+    df_train = df_train[['context', 'label']].groupby(['context'])['label'].apply(lambda x: ';'.join(x)).reset_index()
+    df_train = df_train.drop_duplicates()
+    print(len(df_train))
+
+    print('loaded training dataset ...')
     df_dev = dataset_to_pandas(articles=dev_articles, ref_articles_id=dev_ref_articles_id,
-                                 ref_span_starts=dev_ref_span_starts, ref_span_ends=dev_ref_span_ends,
-                                 train_gold_labels=dev_labels)
-    print('\nBefore transforming to single label / multiclass')
-    print('df_train: {} / df_test: {}'.format(df_train.shape, df_dev.shape))
-    df_train, df_dev = convert_datasets_single_label_multiclass(df_train=df_train, df_test=df_dev)
-    print('\nAfter transforming to single label / multiclass')
-    print('df_train: {} / df_test: {}'.format(df_train.shape, df_dev.shape))
+                               ref_span_starts=dev_ref_span_starts, ref_span_ends=dev_ref_span_ends,
+                               train_gold_labels=dev_labels)
+    unique_contexts = list(set(df_dev['context']))
+    print(len(unique_contexts))  # 4313
+    print(len(df_dev))  # 6128
 
-    # print class percentages in each dataset
-    get_class_percentages(df_train=df_train, df_dev=df_dev)
+    df_dev = df_dev[['context', 'label']].groupby(['context'])['label'].apply(lambda x: ';'.join(x)).reset_index()
+    df_dev = df_dev.drop_duplicates()
+    print(len(df_dev))
+    print('loaded development dataset ...')
+    t2 = time.time()
+    print('time taken: {} mins'.format((t2-t1)/60))
 
-    # since percentages are not equal, will combine both datasets then do a stratified train/test split
     df = pd.concat([df_train, df_dev]).reset_index(drop=True)
     df = df.sample(frac=1).reset_index(drop=True) # shuffle dataframe
-
-    df_train, df_dev = train_test_split(df, test_size=len(df_dev), random_state=42, stratify=df['label'])
-    print('\nAFTER train/test split:\n')
-    get_class_percentages(df_train=df_train, df_dev=df_dev)
-
+    df_train, df_dev = train_test_split(df, test_size=len(df_dev), random_state=42)
     t1 = time.time()
     df_train = translate_dataset(df_train)
     df_dev = translate_dataset(df_dev)
     t2 = time.time()
 
-    print('time taken: {} mins'.format((t2-t1)/60))
-    df_train.to_excel('df_train_single.xlsx', index=False, encoding='utf-8-sig')
-    df_dev.to_excel('df_dev_single.xlsx', index=False, encoding='utf-8-sig')
+    df_train.to_excel('df_train_multi.xlsx', index=False, encoding='utf-8-sig')
+    df_dev.to_excel('df_dev_multi.xlsx', index=False, encoding='utf-8-sig')
+
+    # if os.path.isfile('df_train_single.xlsx'):
+    #     df_train = pd.read_excel('df_train_single.xlsx')
+    #     df_dev = pd.read_excel('df_dev_single.xlsx')
+    #
+    #     print('df_train: {}\ndf_dev:{}'.format(df_train.shape, df_dev.shape))
+    #     get_class_percentages(df=df_train, label_col='label', mode='train')
+    #     get_class_percentages(df=df_dev, label_col='label', mode='validation')
+    #
+    #     get_class_counts(df=df_train, label_col='label', mode='train')
+    #     get_class_counts(df_dev, label_col='label', mode='validation')
+    # else:
+    #     print(os.getcwd())
+    #     translator = Translator()
+    #
+    #     train_data_folder = 'train-articles/'
+    #     dev_data_folder = 'dev-articles/'
+    #     labels_path_train = 'train-task-flc-tc.labels'
+    #     labels_path_dev = 'dev-task-flc-tc.labels'
+    #
+    #     train_articles, train_ref_articles_id, train_ref_span_starts, train_ref_span_ends, train_labels = load_data(train_data_folder, labels_path_train)
+    #     dev_articles, dev_ref_articles_id, dev_ref_span_starts, dev_ref_span_ends, dev_labels = load_data(dev_data_folder, labels_path_dev)
+    #
+    #     df_train = dataset_to_pandas(articles=train_articles, ref_articles_id=train_ref_articles_id,
+    #                                  ref_span_starts=train_ref_span_starts, ref_span_ends=train_ref_span_ends,
+    #                                  train_gold_labels=train_labels)
+    #
+    #     df_dev = dataset_to_pandas(articles=dev_articles, ref_articles_id=dev_ref_articles_id,
+    #                                  ref_span_starts=dev_ref_span_starts, ref_span_ends=dev_ref_span_ends,
+    #                                  train_gold_labels=dev_labels)
+    #     print('\nBefore transforming to single label / multiclass')
+    #     print('df_train: {} / df_test: {}'.format(df_train.shape, df_dev.shape))
+    #     df_train, df_dev = convert_datasets_single_label_multiclass(df_train=df_train, df_test=df_dev)
+    #     print('\nAfter transforming to single label / multiclass')
+    #     print('df_train: {} / df_test: {}'.format(df_train.shape, df_dev.shape))
+    #
+    #     # print class percentages in each dataset
+    #     # get_class_percentages(df_train=df_train, df_dev=df_dev)
+    #     get_class_percentages(df=df_train, label_col='label', mode='train')
+    #     get_class_percentages(df=df_dev, label_col='label', mode='validation')
+    #
+    #     get_class_counts(df=df_train, label_col='label', mode='train')
+    #     get_class_counts(df_dev, label_col='label', mode='validation')
+    #
+    #     # since percentages are not equal, will combine both datasets then do a stratified train/test split
+    #     df = pd.concat([df_train, df_dev]).reset_index(drop=True)
+    #     df = df.sample(frac=1).reset_index(drop=True) # shuffle dataframe
+    #
+    #     df_train, df_dev = train_test_split(df, test_size=len(df_dev), random_state=42, stratify=df['label'])
+    #     print('\nAFTER train/test split:\n')
+    #     # get_class_percentages(df_train=df_train, df_dev=df_dev)
+    #     get_class_percentages(df=df_train, label_col='label', mode='train')
+    #     get_class_percentages(df=df_dev, label_col='label', mode='validation')
+    #
+    #     get_class_counts(df=df_train, label_col='label', mode='train')
+    #     get_class_counts(df_dev, label_col='label', mode='validation')
+    #
+    #     t1 = time.time()
+    #     df_train = translate_dataset(df_train)
+    #     df_dev = translate_dataset(df_dev)
+    #     t2 = time.time()
+    #
+    #     print('time taken: {} mins'.format((t2-t1)/60))
+    #     df_train.to_excel('df_train_single.xlsx', index=False, encoding='utf-8-sig')
+    #     df_dev.to_excel('df_dev_single.xlsx', index=False, encoding='utf-8-sig')
