e022ae9ab9067ef24340cee7b7cfb4c48a1721fc
Hiyam G <hkg02@mail.aub.edu>
2022-10-16 20:15:06 +0300

    craeted code to split data between 3 vs 8 classes for classification

--- get-discourse-datasets/TextClassification/sentences/convert_sentences_to_datasets.py
+++ get-discourse-datasets/TextClassification/sentences/convert_sentences_to_datasets.py
@@ -1,50 +1,59 @@
 import pandas as pd
 from googletrans import Translator
-import json, time
+import json, time, os
 from tqdm import tqdm
 
 
-sentences, labels, labels_general = [], [], []
-labels2gen = {
-    'Main': 'main_contents',
-    'Main_Consequence': 'main_contents',
-    'Cause_Specific': 'context_informing_contents',
-    'Cause_General': 'context_informing_contents',
-    'Distant_Historical': 'additional_supportive_contents',
-    'Distant_Anecdotal': 'additional_supportive_contents',
-    'Distant_Evaluation': 'additional_supportive_contents',
-    'Distant_Expectations_Consequences': 'additional_supportive_contents',
-}
-with open('sentences.json', encoding="utf-8") as json_file:
-    data = json.load(json_file)
-
-    for num in data:
-        sentence = data[num]['sentence'].strip()
-        label = data[num]['label'].strip()
-        if label != 'NAN':
-            label_general = labels2gen[label]
-
-            sentences.append(sentence)
-            labels.append(label)
-            labels_general.append(label_general)
-
-df = pd.DataFrame()
-df['Sentence'] = sentences
-df['Label'] = labels
-df['Label_general'] = labels_general
-df.to_excel('sentences.xlsx', index=False)
+def mkdir(folder):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
 
 
-# Make an English version
-translator = Translator()
-sentences_en = []
-for sent in tqdm(sentences):
-    sent_en = translator.translate(sent, src='ar', dest='en').text
-    sentences_en.append(sent_en)
-    time.sleep(1)
+if __name__ == '__main__':
 
-df = pd.DataFrame()
-df['Sentence'] = sentences_en
-df['Label'] = labels
-df['Label_general'] = labels_general
-df.to_csv('sentences.csv', index=False)
+    sentences, labels, labels_general = [], [], []
+    save_dir = 'csv_annotated/'
+    labels2gen = {
+        'Main': 'main_contents',
+        'Main_Consequence': 'main_contents',
+        'Cause_Specific': 'context_informing_contents',
+        'Cause_General': 'context_informing_contents',
+        'Distant_Historical': 'additional_supportive_contents',
+        'Distant_Anecdotal': 'additional_supportive_contents',
+        'Distant_Evaluation': 'additional_supportive_contents',
+        'Distant_Expectations_Consequences': 'additional_supportive_contents',
+    }
+    with open('sentences.json', encoding="utf-8") as json_file:
+        data = json.load(json_file)
+
+        for num in data:
+            sentence = data[num]['sentence'].strip()
+            label = data[num]['label'].strip()
+            if label != 'NAN':
+                label_general = labels2gen[label]
+
+                sentences.append(sentence)
+                labels.append(label)
+                labels_general.append(label_general)
+
+    df = pd.DataFrame()
+    df['Sentence'] = sentences
+    df['Label'] = labels
+    df['Label_general'] = labels_general
+    mkdir(save_dir)
+    df.to_excel(os.path.join(save_dir, 'sentences.xlsx'), index=False)
+
+    # Make an English version
+    translator = Translator()
+    sentences_en = []
+    for sent in tqdm(sentences):
+        sent_en = translator.translate(sent, src='ar', dest='en').text
+        sentences_en.append(sent_en)
+        time.sleep(1)
+
+    df = pd.DataFrame()
+    df['Sentence'] = sentences_en
+    df['Label'] = labels
+    df['Label_general'] = labels_general
+    mkdir(save_dir)
+    df.to_csv(os.path.join(save_dir, 'sentences.csv'), index=False)
---
+++ get-discourse-datasets/TextClassification/sentences/split.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import os
+
+
+def mkdir(folder):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+
+if __name__ == '__main__':
+
+    save_dir1 = 'csv_annotated/split_specific/'
+    save_dir2 = 'csv_annotated/split_general/'
+
+    # stratified split - according to the 8 labels
+    df = pd.read_csv('csv_annotated/sentences.csv')
+    df_train, df_test = train_test_split(df, test_size=0.3, random_state=42, stratify=df['Label'])
+
+    mkdir(save_dir1)
+    df_train.to_csv(os.path.join(save_dir1, 'sentences_train8.csv'), index=False)
+    df_test.to_csv(os.path.join(save_dir1, 'sentences_test8.csv'), index=False)
+
+    # stratified split - according to 3 labels
+    df = pd.read_csv('csv_annotated/sentences.csv')
+    df_train, df_test = train_test_split(df, test_size=0.3, random_state=42, stratify=df['Label_general'])
+
+    mkdir(save_dir2)
+    df_train.to_csv(os.path.join(save_dir2, 'sentences_train3.csv'), index=False)
+    df_test.to_csv(os.path.join(save_dir2, 'sentences_test3.csv'), index=False)
