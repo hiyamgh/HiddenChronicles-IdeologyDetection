8bfb81649cedccc348e18d51ca5d9fd50e062c1f
Hiyam Ghannam <hkg02@mail.aub.edu>
2022-12-07 09:33:18 +0200

    refactored code to get best accuracies

--- MAML-MultiCross/get_best_accuracy.py
+++ MAML-MultiCross/get_best_accuracy.py
@@ -2,7 +2,10 @@ import numpy as np
 
 # with open('201646.out', 'r') as f: # 0.6547619047619048
 # with open('201648.out', 'r') as f: # 0.6547619047619048
-with open('201647.out', 'r') as f: # 0.7142857142857143
+# with open('201647.out', 'r') as f: # 0.7142857142857143
+# with open('201758.out', 'r') as f: # 0.6309523809523809 This is the run after fixing bug in labeling, choosing the 'best' model
+# with open('201759.out', 'r') as f: # Best accuracy: 0.6547619047619048 This is the run after fixing bug in labeling, choosing the 'latest' model
+with open('201762.out', 'r') as f: # Best accuracy: 0.6666666666666666 This is the run after fixing bug in labeling, choosing the 'latest' model (1st 3 experiments)
     accuracies = []
     best_acc = 0.0
     lines = f.readlines()
