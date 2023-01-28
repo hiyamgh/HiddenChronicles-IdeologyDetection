1a68234a18a9b6f843cf3220d153823b8c5c91fb
Hiyam G <hkg02@mail.aub.edu>
2023-01-28 20:39:58 +0200

    adde code for translating Discourse Profiling data

---
+++ get-discourse-datasets-v2/Discourse_Profiling-master (2)/code/process_file.py
@@ -0,0 +1,79 @@
+from collections import OrderedDict
+import random
+
+
+def sort_order(sids):
+    temp = sorted([int(elem[1:]) for elem in sids])
+    return ['S'+str(elem) for elem in temp]
+
+class Document(object):
+
+    def __init__(self, fname, domain):
+        self.name = fname.split('/')[-1]
+        self.domain = domain
+        self.url = 'None'
+        self.headline = ['headline']
+        self.lead = []
+        self.sentences = OrderedDict()
+        self.tags = dict()
+        self.sent_to_speech = None
+        self.sent_to_event = None
+        self.sids = []
+
+
+def process_doc(fname, domain):
+
+    # Process text document
+    f = open(fname, 'r', encoding='utf-8')
+    # print(fname)
+    doc = Document(fname, domain)
+    lead_para = False
+    sids = []
+    for line in f:
+        temp = line.strip()
+        if temp == '':
+            if lead_para == True:
+                for key in doc.sentences:
+                    doc.lead += doc.sentences[key]
+            lead_para = False
+            continue
+
+        temp = temp.split()
+        if temp[0] == 'URL':
+            doc.url = temp[1]
+        elif temp[0] == 'DATE/':
+            pass
+        elif temp[0] == 'H':
+            if len(temp[1:]) > 0:
+                doc.headline = temp[1:]
+        else:
+            if temp[0] == 'S1':
+                lead_para = True
+            doc.sentences[temp[0]] = temp[1:]
+            sids.append(temp[0])
+
+    # Process annotation file
+    f = open(fname[:-3]+'ann')
+    prev_label = "headline"
+    sent_to_event = dict()
+    sent_to_speech = dict()
+    for line in f:
+        temp = line.strip().split('\t')
+        if len(temp) == 3:
+            label = temp[1].split()[0]
+            if label == 'Speech':
+                sent_to_speech[temp[2]] = label
+            else:
+                # print(temp)
+                sent_to_event[temp[2]] = label
+                
+    doc.sent_to_event = sent_to_event
+    doc.sent_to_speech = sent_to_speech
+    doc.sids = sort_order(sids)
+    first_sentence = True
+    assert(len(sent_to_event) == len(doc.sids))
+
+    return doc
+
+#process_doc('../data/kbp/NYT_ENG_20130912.0240.txt')
+
---
+++ get-discourse-datasets-v2/Discourse_Profiling-master (2)/code/translate_annotations_dev.py
@@ -0,0 +1,158 @@
+import time
+import argparse
+import pandas as pd
+import os
+from googletrans import Translator
+from tqdm import tqdm
+from time import sleep
+
+
+def mkdir(folder):
+    if not os.path.exists(folder):
+        os.makedirs(folder, exist_ok=True)
+
+# https://py-googletrans.readthedocs.io/en/latest/
+# French, fr
+# Spanish, es
+# German, de
+# Greek, el
+# Bulgarian, bg
+# Russian, ru
+# Turkish, tr
+# Vietnamese, vi
+# Thai, th
+# Chinese, zh-cn
+# Hindi, hi
+# Swahili, sw
+# Urdu, ur
+
+codes2names = {
+    'fr': 'french',
+    'es': 'spanish',
+    'de': 'german',
+    'el': 'greek',
+    'bg': 'bulgarian',
+    'ru': 'russian',
+    'tr': 'turkish',
+    'ar': 'arabic',
+    'vi': 'vietnamese',
+    'th': 'thai',
+    'zh-cn': 'chinese (simplified)',
+    'hi': 'hindi',
+    'sw': 'swahili',
+    'ur': 'urdu'
+}
+
+# languages_codes = ['fr', 'es', 'de', 'el', 'bg', 'ru', 'tr', 'vi', 'th', 'zh-cn', 'hi', 'sw', 'ur']
+# languages_names = ['french', 'spanish', 'german', 'greek', 'bulgarian', 'russian', 'turkish', 'vietnamese',
+#                   'thai', 'chinese (simplified)', 'hindi', 'swahili', 'urdu']
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    # parser.add_argument('--lang', type=str, default='fr', help='code of the language of translation')
+    parser.add_argument('--array_id', type=int, default=0, help='SLURM array job id')
+    args = parser.parse_args()
+
+    save_dir = 'translationsv2/'
+    mkdir(save_dir)
+
+    codes = list(codes2names.keys())
+    # get the language from the SLURM ARRAY TASK ID
+    if args.array_id < len(codes2names):
+        lang = codes[args.array_id]
+    elif args.array_id in list(range(len(codes2names), 2*len(codes2names))):
+        lang = codes[args.array_id - len(codes2names)]
+    else:
+        lang = codes[args.array_id - 2*len(codes2names)]
+
+    print('SLURM ARRAY TASK ID: {}, LANGUAGE: {}'.format(args.array_id, lang))
+
+    df = pd.read_csv('df_validation.csv')
+    print('df.shape before dropping duplicates: {}'.format(df.shape))
+    df = df.drop_duplicates()
+    print('df.shape after dropping duplicates: {}'.format(df.shape))
+    translator = Translator()
+
+    df_trans = pd.DataFrame(columns=['Original', 'Sentence', 'Label', 'Label_general', 'Speech_label'])
+    df_to_save_name = 'df_validation_{}.xlsx'.format(lang)
+
+    if os.path.isfile(os.path.join(save_dir, df_to_save_name)):
+        df_trans = pd.read_excel(os.path.join(save_dir, df_to_save_name))
+        num_rows = len(df_trans) # number of rows translated so far
+        if num_rows < len(df):
+            t1 = time.time()
+            for j, row in tqdm(df.iterrows(), total=df.shape[0], desc='Translating to {}'.format(codes2names[lang])):
+                if j >= num_rows:
+                    sentence = row['Sentence'].strip()
+                    if sentence != "":
+                        try:
+                            translated = translator.translate('{}'.format(sentence), src='en', dest=lang).text
+                        except:
+                            try:
+                                print('was not able to translate row {}: {}'.format(j, row['Sentence']))
+                                translator_temp = Translator(service_urls=['translate.googleapis.com'])
+                                translated = translator.translate("{}".format(sentence), dest=lang).text
+                            except:
+                                print('sentence at row {} is set to empty'.format(j))
+                                translated = ""
+                    else:
+                        print('sentence at row {} is empty'.format(j))
+                        translated = ""
+                    df_trans = df_trans.append({
+                        'Original': sentence.strip(),
+                        'Sentence': translated,
+                        'Label': row['Label'],
+                        'Label_general': row['Label_general'],
+                        'Speech_label': row['Speech_label']
+                    }, ignore_index=True)
+
+                    sleep(1 + args.array_id)
+
+                    if j % 50 == 0:
+                        sleep(60)
+
+                    if j % 100 == 0:
+                        df_trans.to_excel(os.path.join(save_dir, df_to_save_name), index=False)
+            t2 = time.time()
+            df_trans.to_excel(os.path.join(save_dir, df_to_save_name), index=False)
+            print('Time taken: {} mins'.format((t2 - t1) / 60))
+        else:
+            print('Completed translating data to {}'.format(lang))
+    else:
+        t1 = time.time()
+        for j, row in tqdm(df.iterrows(), total=df.shape[0], desc='Translating to {}'.format(codes2names[lang])):
+            sentence = row['Sentence'].strip()
+            if sentence != "":
+                try:
+                    translated = translator.translate('{}'.format(sentence), src='en', dest=lang).text
+                except:
+                    try:
+                        print('was not able to translate row {}: {}'.format(j, row['Sentence']))
+                        translator_temp = Translator(service_urls=['translate.googleapis.com'])
+                        translated = translator.translate("{}".format(sentence), dest=lang).text
+                    except:
+                        print('sentence at row {} is set to empty'.format(j))
+                        translated = ""
+            else:
+                print('sentence at row {} is empty'.format(j))
+                translated = ""
+            df_trans = df_trans.append({
+                'Original': sentence.strip(),
+                'Sentence': translated,
+                'Label': row['Label'],
+                'Label_general': row['Label_general'],
+                'Speech_label': row['Speech_label']
+            }, ignore_index=True)
+
+            sleep(1 + args.array_id)
+
+            if j % 50 == 0:
+                sleep(60)
+
+            if j % 100 == 0:
+                df_trans.to_excel(os.path.join(save_dir, df_to_save_name), index=False)
+        t2 = time.time()
+        df_trans.to_excel(os.path.join(save_dir, df_to_save_name), index=False)
+        print('Time taken: {} mins'.format((t2 - t1) / 60))
