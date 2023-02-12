39ec812cd19d539e0404179268de2c9a8c934ca7
Hiyam G <hkg02@mail.aub.edu>
2023-02-12 18:56:11 +0200

    refactored code for creating cross lingual experiments

--- XMAML/create_cross_lingual_exps_1_aux.py
+++ XMAML/create_cross_lingual_exps_1_aux.py
@@ -11,11 +11,31 @@ if __name__ == '__main__':
     save_dir = "cross_lingual_experiments/1_aux/"
     mkdir(save_dir)
 
+    fine_tuning_grid = [1, 0]
+
     # define hyperparameters
-    support_sizes_grid = [8, 16, 32]
-    inner_train_steps_grid = [3, 5, 7, 10]
-    inner_lr_grid = [1e-4, 1e-5, 3e-4, 3e-5, 5e-4, 5e-5]
-    models = ['bert-base-multilingual-cased', 'xlm-roberta-base']
+    # bert base multi lingual
+    BERT_per_gpu_train_batch_sizes_grid = [16]
+    BERT_support_sizes_grid = [8]
+    BERT_inner_train_steps_grid = [3]
+    BERT_inner_lr_grid = [5e-4]
+    BERT_meta_learn_iters = [10, 20, 30, 50, 100, 200, 300, 500]  # will fix the number of iterations (outerloop) for the sake of hyper-parameter tuning
+    models = ['bert-base-multilingual-cased', 'xlm-roberta-base', 'distilbert-base-multilingual-cased']
+    modeltypes = ['bert', 'xlmroberta', 'distilbert']
+
+    #disil
+    DISTIL_per_gpu_train_batch_sizes_grid = [16]
+    DISTIL_support_sizes_grid = [8]
+    DISTIL_inner_train_steps_grid = [5]
+    DISTIL_inner_lr_grid = [1e-4]
+    DISTIL_meta_learn_iters = [10, 20, 30, 50, 100, 200, 300, 500]  # will fix the number of iterations (outerloop) for the sake of hyper-parameter tuning
+
+    # XLM
+    XLM_per_gpu_train_batch_sizes_grid = [16]
+    XLM_support_sizes_grid = [8]
+    XLM_inner_train_steps_grid = [3]
+    XLM_inner_lr_grid = [5e-4]
+    XLM_meta_learn_iters = [10, 20, 30, 50, 100, 200, 300, 500]  # will fix the number of iterations (outerloop) for the sake of hyper-parameter tuning
 
     labels_ARG = "assumption,anecdote,testimony,statistics,common-ground,other"
     labels_ARG_corp = "assumption,statistics,other,testimony,common-ground,anecdote"
@@ -53,48 +73,53 @@ if __name__ == '__main__':
                 dev_dataset_fine_tune_id = ['corp_PRST_ar_VDC']
                 test_dataset_id = ['corp_SSM_ar_VDC']
 
-                for n in support_sizes_grid:
-                    for inner_train_step in inner_train_steps_grid:
-                        for inner_lr in inner_lr_grid:
-                            for model in models:
-                                f.write(
-                                    "--bert_model {} --dev_datasets_ids {} --dev_dataset_finetune {} --test_dataset_eval {} --n {} --inner_train_steps {} --inner_lr {} --labels {}\n".format(
-                                        model,
-                                        ",".join(dev_dataset_ids),
-                                        ",".join(dev_dataset_fine_tune_id),
-                                        ",".join(test_dataset_id),
-                                        n, inner_train_step, inner_lr,
-                                        ",".join(all_labels_VDC)
-                                    ))
+                for n in BERT_support_sizes_grid:
+                    for bs in BERT_per_gpu_train_batch_sizes_grid:
+                        for inner_train_step in BERT_inner_train_steps_grid:
+                            for inner_lr in BERT_inner_lr_grid:
+                                for m_iter in BERT_meta_learn_iters:
+                                    for ft in fine_tuning_grid:
+                                        f.write("--bert_model bert-base-multilingual-cased --model_type bert --dev_datasets_ids {} --dev_dataset_finetune {} --test_dataset_eval {} --do_validation 1 --do_finetuning {} --do_evaluation 1 --meta_learn_iter {} --n {} --per_gpu_train_batch_size {} --inner_train_steps {} --inner_lr {} --labels {} --output_dir_meta {} --eval_task_dir {}\n".format(
+                                            ",".join(dev_dataset_ids),
+                                            ",".join(dev_dataset_fine_tune_id),
+                                            ",".join(test_dataset_id),
+                                            ft, m_iter, n, bs, inner_train_step, inner_lr,
+                                            ",".join(all_labels_VDC),
+                                            "results_cross_lingual/VDC/{}-bert-base-multilingual-cased-m_iter{}-inner_lr{}-inner_train_step{}-n{}-per_gpu_train_batch_size{}-ft{}/".format(lang, m_iter, inner_lr, inner_train_step, n, bs, ft),
+                                            "results_cross_lingaul/VDC/{}-bert-base-multilingual-cased-m_iter{}-inner_lr{}-inner_train_step{}-n{}-per_gpu_train_batch_size{}-ft{}/eval/".format(lang, m_iter, inner_lr, inner_train_step, n, bs, ft)))
+
+                for n in DISTIL_support_sizes_grid:
+                    for bs in DISTIL_per_gpu_train_batch_sizes_grid:
+                        for inner_train_step in DISTIL_inner_train_steps_grid:
+                            for inner_lr in DISTIL_inner_lr_grid:
+                                for m_iter in DISTIL_meta_learn_iters:
+                                    for ft in fine_tuning_grid:
+                                        f.write("--bert_model distilbert-base-multilingual-cased --model_type distilbert --dev_datasets_ids {} --dev_dataset_finetune {} --test_dataset_eval {} --do_validation 1 --do_finetuning {} --do_evaluation 1 --meta_learn_iter {} --n {} --per_gpu_train_batch_size {} --inner_train_steps {} --inner_lr {} --labels {} --output_dir_meta {} --eval_task_dir {}\n".format(
+                                            ",".join(dev_dataset_ids),
+                                            ",".join(dev_dataset_fine_tune_id),
+                                            ",".join(test_dataset_id),
+                                            ft, m_iter, n, bs, inner_train_step, inner_lr,
+                                            ",".join(all_labels_VDC),
+                                            "results_cross_lingual/VDC/{}-distilbert-base-multilingual-cased-m_iter{}-inner_lr{}-inner_train_step{}-n{}-per_gpu_train_batch_size{}-ft{}/".format(lang, m_iter, inner_lr, inner_train_step, n, bs, ft),
+                                            "results_cross_lingaul/VDC/{}-distilbert-base-multilingual-cased-m_iter{}-inner_lr{}-inner_train_step{}-n{}-per_gpu_train_batch_size{}-ft{}/eval/".format(lang, m_iter, inner_lr, inner_train_step, n, bs, ft)))
+
+                for n in XLM_support_sizes_grid:
+                    for bs in XLM_per_gpu_train_batch_sizes_grid:
+                        for inner_train_step in XLM_inner_train_steps_grid:
+                            for inner_lr in XLM_inner_lr_grid:
+                                for m_iter in XLM_meta_learn_iters:
+                                    for ft in fine_tuning_grid:
+                                        f.write("--bert_model xlm-roberta-base --model_type distilbert --dev_datasets_ids {} --dev_dataset_finetune {} --test_dataset_eval {} --do_validation 1 --do_finetuning {} --do_evaluation 1 --meta_learn_iter {} --n {} --per_gpu_train_batch_size {} --inner_train_steps {} --inner_lr {} --labels {} --output_dir_meta {} --eval_task_dir {}\n".format(
+                                            ",".join(dev_dataset_ids),
+                                            ",".join(dev_dataset_fine_tune_id),
+                                            ",".join(test_dataset_id),
+                                            ft, m_iter, n, bs, inner_train_step, inner_lr,
+                                            ",".join(all_labels_VDC),
+                                            "results_cross_lingual/VDC/{}-xlm-roberta-base-m_iter{}-inner_lr{}-inner_train_step{}-n{}-per_gpu_train_batch_size{}-ft{}/".format(lang, m_iter, inner_lr, inner_train_step, n, bs, ft),
+                                            "results_cross_lingaul/VDC/{}-xlm-roberta-base-m_iter{}-inner_lr{}-inner_train_step{}-n{}-per_gpu_train_batch_size{}-ft{}/eval/".format(lang, m_iter, inner_lr, inner_train_step, n, bs, ft)))
+
     f.close()
 
-
-    # Discourse profiling - speeches
-    # (meta train) corp_PRST_aux_VDS / domain 1
-    # (fine tune) corp_PRST_ar_VDS / domain 1
-    # (test) corp_SSM_ar_VDS
-
-    with open(os.path.join(save_dir, 'VDS_cross_lingual.txt'), 'w') as f:
-        for lang in codes2names:
-            if lang != "ar":
-                dev_dataset_ids = ['corp_PRST_{}_VDS'.format(lang)]
-                dev_dataset_fine_tune_id = ['corp_PRST_ar_VDS']
-                test_dataset_id = ['corp_SSM_ar_VDS']
-
-                for n in support_sizes_grid:
-                    for inner_train_step in inner_train_steps_grid:
-                        for inner_lr in inner_lr_grid:
-                            for model in models:
-                                f.write(
-                                    "--bert_model {} --dev_datasets_ids {} --dev_dataset_finetune {} --test_dataset_eval {} --n {} --inner_train_steps {} --inner_lr {} --labels {}\n".format(
-                                        model,
-                                        ",".join(dev_dataset_ids),
-                                        ",".join(dev_dataset_fine_tune_id),
-                                        ",".join(test_dataset_id),
-                                        n, inner_train_step, inner_lr,
-                                        ",".join(all_labels_VDS)
-                                    ))
-    f.close()
 
     # Argumentation
     # (meta train) corp_PRST_aux_ARG / domain 1
@@ -116,44 +141,55 @@ if __name__ == '__main__':
                 dev_dataset_fine_tune_id = ['corp_PRST_ar_ARG']
                 test_dataset_id = ['corp_SSM_ar_ARG']
 
-                for n in support_sizes_grid:
-                    for inner_train_step in inner_train_steps_grid:
-                        for inner_lr in inner_lr_grid:
-                            for model in models:
-                                f.write(
-                                    "--bert_model {} --dev_datasets_ids {} --dev_dataset_finetune {} --test_dataset_eval {} --n {} --inner_train_steps {} --inner_lr {} --labels {}\n".format(
-                                        model,
-                                        ",".join(dev_dataset_ids),
-                                        ",".join(dev_dataset_fine_tune_id),
-                                        ",".join(test_dataset_id),
-                                        n, inner_train_step, inner_lr,
-                                        ",".join(all_labels_ARG)
-                                    ))
-    f.close()
+                for n in BERT_support_sizes_grid:
+                    for bs in BERT_per_gpu_train_batch_sizes_grid:
+                        for inner_train_step in BERT_inner_train_steps_grid:
+                            for inner_lr in BERT_inner_lr_grid:
+                                for m_iter in BERT_meta_learn_iters:
+                                    for ft in fine_tuning_grid:
+                                        f.write("--bert_model bert-base-multilingual-cased --model_type bert --dev_datasets_ids {} --dev_dataset_finetune {} --test_dataset_eval {} --do_validation 1 --do_finetuning {} --do_evaluation 1 --meta_learn_iter {} --n {} --per_gpu_train_batch_size {} --inner_train_steps {} --inner_lr {} --labels {} --output_dir_meta {} --eval_task_dir {}\n".format(
+                                            ",".join(dev_dataset_ids),
+                                            ",".join(dev_dataset_fine_tune_id),
+                                            ",".join(test_dataset_id),
+                                            ft, m_iter, n, bs, inner_train_step, inner_lr,
+                                            ",".join(all_labels_VDC),
+                                            "results_cross_lingual/ARG/{}-bert-base-multilingual-cased-m_iter{}-inner_lr{}-inner_train_step{}-n{}-per_gpu_train_batch_size{}-ft{}/".format(
+                                                lang, m_iter, inner_lr, inner_train_step, n, bs, ft),
+                                            "results_cross_lingaul/ARG/{}-bert-base-multilingual-cased-m_iter{}-inner_lr{}-inner_train_step{}-n{}-per_gpu_train_batch_size{}-ft{}/eval/".format(
+                                                lang, m_iter, inner_lr, inner_train_step, n, bs, ft)))
 
-    # Propaganda
-    # (meta train) corp_PRST_aux_PTC / domain 1
-    # (fine tune) corp_PRST_ar_PTC / domain 2
-    # (test) corp_SSM_ar_PTC
+                for n in DISTIL_support_sizes_grid:
+                    for bs in DISTIL_per_gpu_train_batch_sizes_grid:
+                        for inner_train_step in DISTIL_inner_train_steps_grid:
+                            for inner_lr in DISTIL_inner_lr_grid:
+                                for m_iter in DISTIL_meta_learn_iters:
+                                    for ft in fine_tuning_grid:
+                                        f.write("--bert_model distilbert-base-multilingual-cased --model_type distilbert --dev_datasets_ids {} --dev_dataset_finetune {} --test_dataset_eval {} --do_validation 1 --do_finetuning {} --do_evaluation 1 --meta_learn_iter {} --n {} --per_gpu_train_batch_size {} --inner_train_steps {} --inner_lr {} --labels {} --output_dir_meta {} --eval_task_dir {}\n".format(
+                                            ",".join(dev_dataset_ids),
+                                            ",".join(dev_dataset_fine_tune_id),
+                                            ",".join(test_dataset_id),
+                                            ft, m_iter, n, bs, inner_train_step, inner_lr,
+                                            ",".join(all_labels_VDC),
+                                            "results_cross_lingual/ARG/{}-distilbert-base-multilingual-cased-m_iter{}-inner_lr{}-inner_train_step{}-n{}-per_gpu_train_batch_size{}-ft{}/".format(
+                                                lang, m_iter, inner_lr, inner_train_step, n, bs, ft),
+                                            "results_cross_lingaul/ARG/{}-distilbert-base-multilingual-cased-m_iter{}-inner_lr{}-inner_train_step{}-n{}-per_gpu_train_batch_size{}-ft{}/eval/".format(
+                                                lang, m_iter, inner_lr, inner_train_step, n, bs, ft)))
 
-    with open(os.path.join(save_dir, 'PTC_cross_lingual.txt'), 'w') as f:
-        for lang in codes2names:
-            if lang != "ar":
-                dev_dataset_ids = ['corp_PRST_{}_PTC'.format(lang)]
-                dev_dataset_fine_tune_id = ['corp_PRST_ar_PTC']
-                test_dataset_id = ['corp_SSM_ar_PTC']
+                for n in XLM_support_sizes_grid:
+                    for bs in XLM_per_gpu_train_batch_sizes_grid:
+                        for inner_train_step in XLM_inner_train_steps_grid:
+                            for inner_lr in XLM_inner_lr_grid:
+                                for m_iter in XLM_meta_learn_iters:
+                                    for ft in fine_tuning_grid:
+                                        f.write("--bert_model xlm-roberta-base --model_type distilbert --dev_datasets_ids {} --dev_dataset_finetune {} --test_dataset_eval {} --do_validation 1 --do_finetuning {} --do_evaluation 1 --meta_learn_iter {} --n {} --per_gpu_train_batch_size {} --inner_train_steps {} --inner_lr {} --labels {} --output_dir_meta {} --eval_task_dir {}\n".format(
+                                            ",".join(dev_dataset_ids),
+                                            ",".join(dev_dataset_fine_tune_id),
+                                            ",".join(test_dataset_id),
+                                            ft, m_iter, n, bs, inner_train_step, inner_lr,
+                                            ",".join(all_labels_VDC),
+                                            "results_cross_lingual/ARG/{}-xlm-roberta-base-m_iter{}-inner_lr{}-inner_train_step{}-n{}-per_gpu_train_batch_size{}-ft{}/".format(
+                                                lang, m_iter, inner_lr, inner_train_step, n, bs, ft),
+                                            "results_cross_lingaul/ARG/{}-xlm-roberta-base-m_iter{}-inner_lr{}-inner_train_step{}-n{}-per_gpu_train_batch_size{}-ft{}/eval/".format(
+                                                lang, m_iter, inner_lr, inner_train_step, n, bs, ft)))
 
-                for n in support_sizes_grid:
-                    for inner_train_step in inner_train_steps_grid:
-                        for inner_lr in inner_lr_grid:
-                            for model in models:
-                                f.write(
-                                    "--bert_model {} --dev_datasets_ids {} --dev_dataset_finetune {} --test_dataset_eval {} --n {} --inner_train_steps {} --inner_lr {} --labels {}\n".format(
-                                        model,
-                                        ",".join(dev_dataset_ids),
-                                        ",".join(dev_dataset_fine_tune_id),
-                                        ",".join(test_dataset_id),
-                                        n, inner_train_step, inner_lr,
-                                        ";".join(all_labels_PTC)
-                                    ))
     f.close()
